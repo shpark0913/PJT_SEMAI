443e7246fd68835176a83d4e5d12ccd2ab16f76e
gyur1kim <qri1114@gmail.com>
2023-04-17 16:44:44 +0900

    :sparkles: [FE] 초기 셋팅
    - react typescript로 template 생성
    - axios, redux, router-dom, styled-components 모듈 다운
    - electron.js 설정
    - redux-persist 설정
    - 폴더 구조

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <h1>샘플입니다</h1>
+      <div>샘플입니다</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import { Provider } from 'react-redux';
+import { store } from './_store/store';
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist"
+
+const persistor = persistStore(store);
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
