3ef80243c41defe9144e07a3dd811f4bf79e683c
abin7989 <abin7989@gmail.com>
2023-04-17 13:34:43 +0900

    spring boot 초기설정

---
+++ backend/semes/semes/src/main/java/com/ssafy/semes/SemesApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.semes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemesApplication.class, args);
+	}
+
+}
---
+++ backend/semes/semes/src/test/java/com/ssafy/semes/SemesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.semes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
