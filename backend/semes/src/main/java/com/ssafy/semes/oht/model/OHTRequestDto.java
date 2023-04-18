1a5326bc906f6589184a629474e49079ec1f0b88
안태윤 <cfdw011@gmail.com>
2023-04-19 08:50:05 +0900

    :sparkles: [BE]  OHT Create,Read api
    OHT를 생성하고, 조회하는 API
    
    Closes #S08P31A301-200

---
+++ backend/semes/src/main/java/com/ssafy/semes/oht/model/OHTRequestDto.java
@@ -0,0 +1,10 @@
+package com.ssafy.semes.oht.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OHTRequestDto {
+	public String ohtSN;
+}
--- backend/semes/src/main/java/com/ssafy/semes/oht/model/repository/OHTRepository.java
+++ backend/semes/src/main/java/com/ssafy/semes/oht/model/repository/OHTRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 import com.ssafy.semes.oht.model.OHTEntity;
 @Repository
 public interface OHTRepository extends JpaRepository<OHTEntity,Long> {
+	OHTEntity findByOhtSN(String ohtSN);
 }
