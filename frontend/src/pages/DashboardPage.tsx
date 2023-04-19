102586ceeb33926ac041e94fb659d764b53a400e
shpark0913 <shpark097754@gmail.com>
2023-04-19 17:40:41 +0900

    :sparkles: [FE] Dashboard HTML 초안 작성
    closes #S08P31A301-203

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import styled from "styled-components";
+
+const MainGrid = styled.section`
+  display: grid;
+  width: 100%;
+  height: 95vh;
+  /* grid-template-rows: repeat(4, 25vh);
+  grid-template-columns: ; */
+  gap: 25px;
+`;
+
+const OHTWheel = styled.section`
+  background-color: skyblue;
+  grid-row: ;
+  grid-column: ;
+`;
+
+const OnInspection = styled.section`
+  background-color: greenyellow;
+`;
+
+const Transition = styled.section`
+  background-color: blueviolet;
+`;
+
+const AllOHTWheel = styled.section`
+  background-color: pink;
+`;
 
 function DashboardPage() {
   return (
-    <div>
-      <h1>대시보드 페이지</h1>
-    </div>
+    <MainGrid>
+      <OHTWheel>OHT Wheel 현황</OHTWheel>
+      <OnInspection>검사 중</OnInspection>
+      <Transition>전이 데이터</Transition>
+      <AllOHTWheel>OHT Wheel 전체 검사 현황</AllOHTWheel>
+    </MainGrid>
   );
 }
 
