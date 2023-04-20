1e29cb3cbd96612217b5d820c5713c6aa53d56f8
shpark0913 <shpark097754@gmail.com>
2023-04-20 17:05:35 +0900

    :lipstick: [FE] Dashboard HTML UI 구성 진행
    - 현재 Dashboard HTML UI 구성 중.
    - Grid 및 Flex로 구성 중이며 공통 컴포넌트 제작 중
    
    closes #S08P31A301-218

--- frontend/src/components/DashboardPage/OHTAllResult.tsx
+++ frontend/src/components/DashboardPage/OHTAllResult.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import DashboardTitle from "./DashboardTitle";
+import OHTResultMenuBar from "./OHTResultMenuBar";
+import styled from "styled-components";
+
+const OHTAllResultContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  grid-row: 3/4;
+  grid-column: 1/3;
+  margin-top: 10px;
+`;
+
+const OHTAllResultSec = styled.div`
+  flex: 1;
+`;
 
 function OHTAllResult() {
-  return <div></div>;
+  return (
+    <OHTAllResultContainer>
+      <DashboardTitle title="OHT 휠 검사 현황" />
+      <OHTResultMenuBar />
+      <OHTAllResultSec>안녕</OHTAllResultSec>
+    </OHTAllResultContainer>
+  );
 }
 
 export default OHTAllResult;
