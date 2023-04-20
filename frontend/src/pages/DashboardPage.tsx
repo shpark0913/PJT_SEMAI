1f727e29ef43026cdb091e3a9259930571c16bad
shpark0913 <shpark097754@gmail.com>
2023-04-20 10:32:26 +0900

    :sparkles: [FE] Dashboard HTML 틀 구성
    - Dashboard를 OHTResult, OHTCheck, OHTTransition, OHTAllResult
      Section으로 구분
    
    closes #S08P31A301-214

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -2,38 +2,44 @@ import styled from "styled-components";
 
 const MainGrid = styled.section`
   display: grid;
-  width: 100%;
-  height: 95vh;
-  /* grid-template-rows: repeat(4, 25vh);
-  grid-template-columns: ; */
+  width: 98%;
+  height: 88vh;
+  grid-template-rows: 30% 30% 40%;
+  grid-template-columns: 70% 30%;
   gap: 25px;
 `;
 
-const OHTWheel = styled.section`
-  background-color: skyblue;
-  grid-row: ;
-  grid-column: ;
+const OHTResult = styled.section`
+  background-color: var(--section-color);
+  grid-row: 1/3;
+  grid-column: 1/2;
 `;
 
-const OnInspection = styled.section`
-  background-color: greenyellow;
+const OHTCheck = styled.section`
+  background-color: var(--section-color);
+  grid-row: 1/2;
+  grid-column: 2/3;
 `;
 
-const Transition = styled.section`
-  background-color: blueviolet;
+const OHTTransition = styled.section`
+  background-color: var(--section-color);
+  grid-row: 2/3;
+  grid-column: 2/3;
 `;
 
-const AllOHTWheel = styled.section`
-  background-color: pink;
+const OHTAllResult = styled.section`
+  background-color: var(--section-color);
+  grid-row: 3/4;
+  grid-column: 1/3;
 `;
 
 function DashboardPage() {
   return (
     <MainGrid>
-      <OHTWheel>OHT Wheel 현황</OHTWheel>
-      <OnInspection>검사 중</OnInspection>
-      <Transition>전이 데이터</Transition>
-      <AllOHTWheel>OHT Wheel 전체 검사 현황</AllOHTWheel>
+      <OHTResult>OHT Wheel 현황</OHTResult>
+      <OHTCheck>검사 중</OHTCheck>
+      <OHTTransition>전이 데이터</OHTTransition>
+      <OHTAllResult>OHT Wheel 전체 검사 현황</OHTAllResult>
     </MainGrid>
   );
 }
