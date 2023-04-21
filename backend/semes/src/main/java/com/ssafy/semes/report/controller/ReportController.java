9ff3a17634c69962135cc395db6ad953f89ae30a
abin7989 <abin7989@gmail.com>
2023-04-21 09:01:32 +0900

    :sparkles: [BE] 보고서 검색 구현 S08P31A301-211

--- backend/semes/src/main/java/com/ssafy/semes/report/controller/ReportController.java
+++ backend/semes/src/main/java/com/ssafy/semes/report/controller/ReportController.java
@@ -3,6 +3,7 @@ package com.ssafy.semes.report.controller;
 import com.ssafy.semes.common.ErrorCode;
 import com.ssafy.semes.common.SuccessCode;
 import com.ssafy.semes.common.dto.ApiResponse;
+import com.ssafy.semes.report.model.QuestionDto;
 import com.ssafy.semes.report.model.ReportListResponseDto;
 import com.ssafy.semes.report.model.service.ReportService;
 import lombok.extern.slf4j.Slf4j;
@@ -20,11 +21,12 @@ public class ReportController {
     @Autowired
     private ReportService reportService;
 
-    @GetMapping("/list")
-    private ApiResponse<?> findReport(Pageable pageable, @RequestParam String mode){
+    @PostMapping("/list")
+    private ApiResponse<?> findReport( @RequestBody QuestionDto questionDto){//,
         log.info("Report FindReport Start");
         try {
-            return ApiResponse.success(SuccessCode.REAT_BOARD_LIST,reportService.findReport(pageable));
+
+            return ApiResponse.success(SuccessCode.REAT_BOARD_LIST,reportService.findReport(questionDto));
 
         }catch (Exception e){
             log.error("Report FindReport Error");
