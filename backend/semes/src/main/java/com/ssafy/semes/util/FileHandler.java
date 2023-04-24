1903f5c9d413af6440da6545110ae8cd8a6d9c72
안태윤 <cfdw011@gmail.com>
2023-04-24 17:51:02 +0900

    :sparkles: [BE] OHT 검사 요청시 OHT 검사기록 생성
    OHT serial no 와 file 요청 받는 API
    OHT SN 예외처리
    
    Closes #S08P31A301-379

--- backend/semes/src/main/java/com/ssafy/semes/util/FileHandler.java
+++ backend/semes/src/main/java/com/ssafy/semes/util/FileHandler.java
@@ -1,7 +1,15 @@
 package com.ssafy.semes.util;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+import javax.xml.bind.DatatypeConverter;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,24 +17,74 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class FileHandler {
-	static String BASE_PATH="C:"+ File.separator+"dataset"+"";
-	static public void create(String dir,MultipartFile newFile) throws IOException {
-		// directory 검사
-		File path = new File(dir+ File.separator+newFile.getOriginalFilename());
-		newFile.transferTo(path);
-	}
-	static public void delete(String dir, String fileName){
-		File file = new File(BASE_PATH+File.separator+dir+ File.separator+fileName);
+	static String BASE_PATH = "C:" + File.separator + "dataset" + File.separator;
+	static boolean baseChecked = false;
 
-		if( file.exists() ){
-			if(file.delete()){
-				log.info("파일삭제 성공");
-			}else{
-				log.info("파일삭제 실패");
-			}
-		}else{
-			log.error("파일이 존재하지 않습니다.");
+	static public String create(String dir, String filename, MultipartFile inputFile) throws IOException {
+		StringBuilder sb = new StringBuilder(BASE_PATH);
+		// directory 검사
+		String path = sb.append(dir).append(File.separator).toString();
+		File folder = new File(path);
+		if (!folder.exists()) {
+			folder.mkdirs();
 		}
+		//file 생성
+		sb = new StringBuilder(path);
+		String ext = inputFile.getOriginalFilename().split(".")[1];
+		String dest = sb.append(filename).append('.').append(ext).toString();
+
+		File file = new File(dest);
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+
+		//새파일 이동
+		inputFile.transferTo(file);
+
+		return dest;
+	}
+
+	static public boolean delete(String dir, String fileName) throws FileNotFoundException {
+		File file = new File(BASE_PATH + File.separator + dir + File.separator + fileName);
+
+		if (file.exists()) {
+			if (file.delete()) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			FileNotFoundException e = new FileNotFoundException("파일이 존재하지 않습니다.");
+			return false;
+		}
+	}
+
+	static public String getUniqueFileName(String originName) {
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		String formattedTime = new SimpleDateFormat("yyyy-MM-dd hh:m:ss").format(timestamp);
+
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+
+		String uniqueString = originName + formattedTime;
+		byte[] digest = md.digest(originName.getBytes(StandardCharsets.UTF_8));
+		String sha256 = DatatypeConverter.printHexBinary(digest).toLowerCase();
+
+		return sha256;
+	}
+
+	static public void initBasePath() {
+		File file = new File(BASE_PATH);
+		if (!file.exists()) {
+			baseChecked = file.mkdirs();
+			return;
+		}
+		baseChecked = true;
+		return;
 	}
 
 }
