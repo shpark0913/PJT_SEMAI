299851ce8155d9a7acb3c8760525762c3a1e69c9
abin7989 <abin7989@gmail.com>
2023-04-24 16:10:44 +0900

    :sparkles: [BE] 유저 로그인 구현 S08P31A301-372

--- backend/semes/src/main/java/com/ssafy/semes/user/model/service/UserServiceImpl.java
+++ backend/semes/src/main/java/com/ssafy/semes/user/model/service/UserServiceImpl.java
@@ -1,4 +1,20 @@
 package com.ssafy.semes.user.model.service;
 
+import com.ssafy.semes.user.model.UserEntity;
+import com.ssafy.semes.user.model.UserRequestDto;
+import com.ssafy.semes.user.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl implements UserSerivce{
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserEntity findUser(UserRequestDto userRequestDto) throws Exception {
+        UserEntity user = userRepository.findByUserIdAndUserPwd(
+                userRequestDto.getUserId(),
+                userRequestDto.getUserPwd());
+        return user;
+    }
 }
