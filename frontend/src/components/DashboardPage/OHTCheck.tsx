a234930cd30cb85b916cdefc3573f6e0acc42458
shpark0913 <shpark097754@gmail.com>
2023-04-24 14:46:40 +0900

    :sparkles: [FE] 대시보드 현재 검사 중인 OHT 컴포넌트 생성
    - 컴포넌트 html 구성 완료
    
    closes #S08P31A301-341

--- frontend/src/components/DashboardPage/OHTCheck.tsx
+++ frontend/src/components/DashboardPage/OHTCheck.tsx
@@ -1,7 +1,9 @@
+import { ReactComponent as Check } from "../../assets/Check.svg";
 import DashboardTitle from "./DashboardTitle";
+import { ReactComponent as NotCheck } from "../../assets/NotCheck.svg";
 import styled from "styled-components";
 
-const OHTCheckContainer = styled.section`
+const OHTCheckSec = styled.section`
   display: flex;
   flex-direction: column;
   grid-row: 1/2;
@@ -9,18 +11,97 @@ const OHTCheckContainer = styled.section`
   padding: 10px;
 `;
 
-const OHTCheckSec = styled.div`
+const OHTCheckContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   flex: 1;
   background-color: var(--section-color);
-  height: 90%;
+`;
+
+const OHTCheckTitle = styled.div`
+  display: flex;
+  flex: 0.4;
+  align-items: center;
+  justify-content: center;
+`;
+
+const OHTCheckContent = styled.div`
+  display: flex;
+  background-color: var(--background-color);
+  margin: 0;
+  padding: 10px 25px;
+`;
+
+const OHTCheckGaugeDiv = styled.div`
+  flex: 0.6;
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  position: relative;
+`;
+
+const OHTCheckGauge = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  z-index: 2;
+`;
+
+const OHTCheckBar = styled.div`
+  width: 100%;
+  background-color: var(--emphasize-color);
+  position: absolute;
+  height: 7px;
+  top: 29%;
+  z-index: 1;
 `;
 
 function OHTCheck() {
+  const checkBar: boolean[] = [true, true, false, false];
+  console.log(checkBar);
+  console.log("3번째 : ", checkBar[3]);
+  // switch (checkBar) {
+  //   case checkBar[3] === true:
+  //     console.log(2)
+  //     break
+  // }
+
   return (
-    <OHTCheckContainer>
+    <OHTCheckSec>
       <DashboardTitle title="현재 검사 OHT" />
-      <OHTCheckSec>안녕</OHTCheckSec>
-    </OHTCheckContainer>
+      <OHTCheckContainer>
+        <OHTCheckTitle>
+          <OHTCheckContent style={{ color: "var(--emphasize-color)", marginRight: "5px" }}>
+            호기 ID
+          </OHTCheckContent>
+          <OHTCheckContent>V30001-FL-1681704285</OHTCheckContent>
+        </OHTCheckTitle>
+        <OHTCheckGaugeDiv>
+          <OHTCheckBar>&nbsp;</OHTCheckBar>
+
+          <OHTCheckGauge>
+            <Check />
+            <p>FL</p>
+          </OHTCheckGauge>
+
+          <OHTCheckGauge>
+            <Check />
+            <p>FF</p>
+          </OHTCheckGauge>
+
+          <OHTCheckGauge>
+            <Check />
+            <p>RL</p>
+          </OHTCheckGauge>
+
+          <OHTCheckGauge>
+            <NotCheck />
+            <p>RR</p>
+          </OHTCheckGauge>
+        </OHTCheckGaugeDiv>
+      </OHTCheckContainer>
+    </OHTCheckSec>
   );
 }
 
