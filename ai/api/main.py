2bccfb2eb2bca8b3e09722b5b3f18fb47c71195f
김영식 <kimyoungsik7@naver.com>
2023-04-24 17:08:10 +0900

    :sparkles:[BE] Fast API : 분류 모델 통신 코드 구현
    이미지 분류 요청 시 200, 400, 500 코 드에 따른 return 값 설정 완료
    
    closes #S08P31A301-388

--- ai/api/main.py
+++ ai/api/main.py
@@ -7,8 +7,7 @@ import os
 import io
 import torch
 import torchvision.transforms as transforms
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, HTTPException
 from PIL import Image
 
 # GPU가 사용 가능한 경우 cuda를 0으로 초기화하여 사용 / GPU가 사용 불가능한 경우 CPU로 초기화하여 CPU 사용
@@ -57,7 +56,7 @@ transformations = transforms.Compose([
 ])
 
 # 볼트 이미지를 읽어 결과를 반환하는 함수
-def get_prediction(image):
+def classification(image):
     # 이미지를 전처리(unsqueeze를 이용해 배치 차원을 추가하고, GPU를 사용)
     image = transformations(image).unsqueeze(0).to(device)
 
@@ -75,13 +74,33 @@ def get_prediction(image):
 @app.get("/infer")
 # 휠 이미지 디텍션 후 볼트 분류 함수 실행(쿼리에 담긴 filePath 전달)
 def detect_classification(filePath: str):
-    # filePath의 앞부분을 IMG_DIR에 저장
-    IMG_DIR = os.path.dirname('../../semes_bolt/')
-    # image에 해당 휠 이미지 열기
-    image = Image.open(os.path.join(IMG_DIR, filePath))
-    # image를 RGB로 변환
-    image = image.convert('RGB')
-    # 볼트 분류 결과를 class_name에 저장
-    class_name = get_prediction(image)
-    print(class_name)
-    return class_name
+    try:
+        # filePath의 앞부분을 IMG_DIR에 저장
+        IMG_DIR = os.path.dirname('../../semes_bolt/')
+        # image에 해당 휠 이미지 열기
+        image = Image.open(os.path.join(IMG_DIR, filePath))
+        # image를 RGB로 변환
+        image = image.convert('RGB')
+        # 볼트 분류 결과를 class_name에 저장
+        class_name = classification(image)
+        print(class_name)
+        
+        # 데이터를 JSON 형식으로 구성
+        data = {
+            "markedImage": "WHEEL_RESULT/marked.png",
+            "bolts": [],
+            "word": "저장중"
+        }
+        
+        # 성공적으로 분류 작업을 수행한 경우
+        return {
+            "status": 200,
+            "success": True,
+            "data": data
+        }
+    except FileNotFoundError:
+        # 파일 경로가 잘못된 경우
+        raise HTTPException(status_code=400, detail="필요한 값이 없습니다.")
+    except Exception as e:
+        # 분류 작업 중 오류가 발생한 경우
+        raise HTTPException(status_code=500, detail="서버 내 오류")
