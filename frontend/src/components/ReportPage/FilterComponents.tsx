23774d8943d38e7a6826bbbd376a6f9e1de25180
gyur1kim <qri1114@gmail.com>
2023-04-25 15:58:13 +0900

    :lipstick: [FE] input에 쓰이는 svg 색상 변경
    - 현재 테마를 props로 넘긴다.
    - 테마가 dark인지 light인지에 따라 fill을 다르게 지정
    
    closes #S08P31A301-

--- frontend/src/components/ReportPage/FilterComponents.tsx
+++ frontend/src/components/ReportPage/FilterComponents.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
-const Label = styled.label`
+type LabelProps = {
+  theme: string;
+}
+const Label = styled.label<LabelProps>`
   color: var(--emphasize-color);
   margin-right: 25px;
   height: 100%;
@@ -21,10 +24,10 @@ const Label = styled.label`
   & > select {
     appearance:none;
     background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20">
-  <path fill="${encodeURIComponent('#fff')}" 
-  d="m7 10 5 5 5-5z"/>
-</svg>') calc(100% - 5px) calc(100% - 7px) no-repeat;
-    
+                  <path 
+                    fill="${props => props.theme === "dark" ? encodeURIComponent('#A3D1FF') : encodeURIComponent('#0052A4')}" 
+                    d="m7 10 5 5 5-5z"/>
+                  </svg>') calc(100% - 5px) calc(100% - 7px) no-repeat;
     & > option {
       background-color: var(--background-color);
     }
@@ -32,13 +35,11 @@ const Label = styled.label`
   
   // calendar CSS
   & > input[type="date"]::-webkit-calendar-picker-indicator {
-    //color: rgba(0, 0, 0, 0);
-    //opacity: 1;
     background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="15" width="15" viewBox="0 0 25 25">
-  <path fill="${encodeURIComponent('#fff')}" 
-  d="M22 3h-3V1h-2v2H7V1H5v2H2v20h20V3zm-2 18H4V8h16v13z" />
-</svg>') left center no-repeat;
-    //border-width: thin;
+                  <path 
+                    fill="${props => props.theme === "dark" ? encodeURIComponent('#A3D1FF') : encodeURIComponent('#0052A4')}" 
+                    d="M22 3h-3V1h-2v2H7V1H5v2H2v20h20V3zm-2 18H4V8h16v13z"/>
+                  </svg>') left center no-repeat;
   }
 `
 
