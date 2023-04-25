3e3578ef96b9022ad3b35432947168e50a56ea86
gyur1kim <qri1114@gmail.com>
2023-04-25 11:04:33 +0900

    :lipstick: [FE] input 태그의 아이콘 변경
    - 기존의 화살표와 캘린더 아이콘 삭제
    - mui 파일을 직접 background-image의 url값으로 넣어서 해결함
      - mui 아이콘을 url로 넣는 방법은 트러블슈팅 작성함
      - 아직 svg파일 색상이 변경되는 것은 구현하지 않음
    
    closes #S08P31A301-

--- frontend/src/components/ReportPage/FilterComponents.tsx
+++ frontend/src/components/ReportPage/FilterComponents.tsx
@@ -14,22 +14,31 @@ const Label = styled.label`
     border-radius: 5px;
     border: 1px solid var(--emphasize-color);
     margin-left: 10px;
-    padding: 7px;
+    padding: 5px;
   }
   
   // 셀렉트 CSS
   & > select {
     appearance:none;
+    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20">
+  <path fill="${encodeURIComponent('#fff')}" 
+  d="m7 10 5 5 5-5z"/>
+</svg>') calc(100% - 5px) calc(100% - 7px) no-repeat;
     
     & > option {
       background-color: var(--background-color);
-      padding: 50px;
     }
   }
   
   // calendar CSS
-  & > input[type="date"] {
-
+  & > input[type="date"]::-webkit-calendar-picker-indicator {
+    //color: rgba(0, 0, 0, 0);
+    //opacity: 1;
+    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="15" width="15" viewBox="0 0 25 25">
+  <path fill="${encodeURIComponent('#fff')}" 
+  d="M22 3h-3V1h-2v2H7V1H5v2H2v20h20V3zm-2 18H4V8h16v13z" />
+</svg>') left center no-repeat;
+    //border-width: thin;
   }
 `
 
