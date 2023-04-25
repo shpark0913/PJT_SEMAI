342f09fc0121ea5c378244edc8495904ae17c0df
gyur1kim <qri1114@gmail.com>
2023-04-25 16:05:16 +0900

    :lipstick: [FE] report css
    -
    
    closes #S08P31A301-293

--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -2,7 +2,7 @@ import React, { useState} from 'react';
 import { Form } from "react-router-dom";
 import styled from "styled-components";
 
-import { Button } from "../components/ButtonComponents";
+import {Button, SemesButton} from "../components/ButtonComponents";
 import { Label } from "../components/ReportPage/FilterComponents"
 import ReportTable from "../components/ReportPage/ReportTable";
 import {useSelector} from "react-redux";
@@ -34,43 +34,49 @@ function ReportPage() {
   return (
     <ReportSection>
       <h1>레포트</h1>
-      <Form style={{height :"30px", marginBottom: "10px"}}>
-        <Label theme={theme}>
-          장비 종류
-          <select name="ohtSn">
-            <option value="ALL">전체</option>
-            <option value="V30001">V30001</option>
-            <option value="V30002">V30002</option>
-            <option value="V30003">V30003</option>
-          </select>
-        </Label>
-        <Label theme={theme}>
-          검사 일자
-          <input type="date" value={chosenDate} name="date" max={TodayDate} onChange={e => handleChange(e)} />
-        </Label>
-        <Label theme={theme}>
-          검사 시간
-          <select name="time">
-            <option value="ALL">전체</option>
-            <option value="0">00:00</option>
-            <option value="1">01:00</option>
-            <option value="2">02:00</option>
-            <option value="23">23:00</option>
-          </select>
-        </Label>
-        <Label theme={theme}>
-          검사 휠 위치
-          <select name="wheelPosition">
-            <option value="ALL">전체</option>
-            <option value="FL">FL</option>
-            <option value="FR">FR</option>
-            <option value="RL">RL</option>
-            <option value="RR">RR</option>
-          </select>
-        </Label>
+      <Form style={{height :"30px", marginBottom: "15px", display: "flex", justifyContent: "space-between"}}>
+        <div>
+          <Label theme={theme}>
+            장비 종류
+            <select name="ohtSn">
+              <option value="ALL">전체</option>
+              <option value="V30001">V30001</option>
+              <option value="V30002">V30002</option>
+              <option value="V30003">V30003</option>
+            </select>
+          </Label>
+          <Label theme={theme}>
+            검사 일자
+            <input type="date" value={chosenDate} name="date" max={TodayDate} onChange={e => handleChange(e)} />
+          </Label>
+          <Label theme={theme}>
+            검사 시간
+            <select name="time">
+              <option value="ALL">전체</option>
+              <option value="0">00:00</option>
+              <option value="1">01:00</option>
+              <option value="2">02:00</option>
+              <option value="23">23:00</option>
+            </select>
+          </Label>
+          <Label theme={theme}>
+            검사 휠 위치
+            <select name="wheelPosition">
+              <option value="ALL">전체</option>
+              <option value="FL">FL</option>
+              <option value="FR">FR</option>
+              <option value="RL">RL</option>
+              <option value="RR">RR</option>
+            </select>
+          </Label>
+          <SemesButton width="90px" height="100%" type="submit">조회하기</SemesButton>
+        </div>
 
-        <Button width="90px" height="100%" type="submit">조회하기</Button>
+        <div>
+          <Button width="90px" height="100%" type="submit">CSV 출력</Button>
+        </div>
       </Form>
+
       <ReportTable />
     </ReportSection>
   );
