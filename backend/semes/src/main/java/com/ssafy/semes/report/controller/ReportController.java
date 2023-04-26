0a71fb2a0cebd07360c5989ead96387828535aea
abin7989 <abin7989@gmail.com>
2023-04-26 09:42:39 +0900

    :sparkles: [BE] 슬랙 메시지 비동기 처리 S08P31A301-372
    Former-commit-id: f34e15293ab0ab5c638f5fc54ced697a01b688e1

--- backend/semes/src/main/java/com/ssafy/semes/report/controller/ReportController.java
+++ backend/semes/src/main/java/com/ssafy/semes/report/controller/ReportController.java
@@ -4,16 +4,11 @@ import com.ssafy.semes.common.ErrorCode;
 import com.ssafy.semes.common.SuccessCode;
 import com.ssafy.semes.common.dto.ApiResponse;
 import com.ssafy.semes.report.model.QuestionDto;
-import com.ssafy.semes.report.model.ReportListResponseDto;
 import com.ssafy.semes.report.model.service.ReportService;
-import com.ssafy.semes.util.Slack;
+import com.ssafy.semes.util.SlackController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/report")
@@ -21,7 +16,9 @@ import java.util.List;
 public class ReportController {
     @Autowired
     private ReportService reportService;
-    private Slack slack = new Slack();
+
+    @Autowired
+    private SlackController slackController;
 
     @PostMapping("/list")
     private ApiResponse<?> findReport(@RequestBody QuestionDto questionDto){
@@ -40,7 +37,7 @@ public class ReportController {
         try{
             return ApiResponse.success(SuccessCode.READ_REPORT_DETAIL, reportService.findReportDetail(wheelChcekId));
         }catch (Exception e){
-            slack.send("Report findReportDetail Error : "+e.getMessage());
+            slackController.send("Report findReportDetail Error : "+e.getMessage());
             log.error("Report findReportDetail Error : "+e.getMessage());
             return ApiResponse.error(ErrorCode.INTERNAL_SERVER_EXCEPTION);
         }
