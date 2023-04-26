f3f5077db5596a0f9300e6f22c485348d65cc648
gyur1kim <qri1114@gmail.com>
2023-04-26 15:14:11 +0900

    :lipstick: [FE] 레포트 상세보기 모달창 CSS 완료
    - flex를 이용한 정렬 작업 완료
    
    closes #S08P31A301-315
    
    
    Former-commit-id: a0afba8adfcbc586fc3acd9e75c4a022e9a6f558

--- frontend/src/components/DetailModal/DetailModal.tsx
+++ frontend/src/components/DetailModal/DetailModal.tsx
@@ -4,18 +4,24 @@ import { DetailModalProps } from "../../_utils/Types";
 import Title from "../Title";
 import {Button} from "../ButtonComponents";
 import DetailTable from "./DetailTable";
+import styled from "styled-components";
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 
+const TitleButtonDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+`
 function DetailModal({ detailInfo, setIsModalOpen }: DetailModalProps) {
-
   return (
     <div>
       <ModalBackground />
       <DetailContainer>
-        <CloseButton onClick={e => setIsModalOpen(false)}>{`>>`}</CloseButton>
-        <div>
+        <CloseButton onClick={e => setIsModalOpen(false)}><KeyboardDoubleArrowRightIcon sx={{height: "35px", width: "35px"}} /></CloseButton>
+        <TitleButtonDiv>
           <Title title="레포트 상세보기" />
           <Button width="90px" height="25px">CSV 출력</Button>
-        </div>
+        </TitleButtonDiv>
         <DetailTable detailInfo={detailInfo} />
       </DetailContainer>
     </div>
