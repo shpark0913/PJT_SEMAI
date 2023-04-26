ec07cac58721e70435ef82169d8b92ad13a1c01c
shpark0913 <shpark097754@gmail.com>
2023-04-26 11:10:26 +0900

    :lipstick: [FE] 로그인 화면 UI 완료
    - 로그인 화면 로고, ID, PASSWORD HTML, CSS 완료
    
    closes #S08P31A301-347
    
    
    Former-commit-id: cf10b551e4b3b261d470354ad4df5cbcf8043273

--- frontend/src/components/LoginPage/Login.js
+++ frontend/src/components/LoginPage/Login.js
@@ -1,17 +1,20 @@
 import "./Login.css";
 
-import Logo from "../../assets/semesLogo.png";
+import LoginInput from "./LoginInput";
+import Logo from "../../assets/semesLogoWhite.png";
+import styled from "styled-components";
+
+const LoginDiv = styled.div`
+  background-color: white;
+`;
 
 function Login() {
   return (
     <div>
       <div class="header">
         <div class="inner-header flex">
-          <img
-            src={Logo}
-            alt=""
-            style={{ width: "22%", filter: "opacity(0.5) drop-shadow(0 0 0 white)" }}
-          />
+          <img src={Logo} alt="" style={{ width: "22%" }} />
+          <LoginInput />
         </div>
 
         <div>
@@ -30,10 +33,10 @@ function Login() {
               />
             </defs>
             <g class="parallax">
-              <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7)" />
+              <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(214,235,255,0.7)" />
               <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
               <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
-              <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
+              <use xlinkHref="#gentle-wave" x="48" y="7" fill="#D6EBFF" />
             </g>
           </svg>
         </div>
