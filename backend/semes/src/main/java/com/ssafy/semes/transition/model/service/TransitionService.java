0945216fe23a4ad88f0f853cafbb746abbaedca1
안태윤 <cfdw011@gmail.com>
2023-04-26 10:49:23 +0900

    :sparkles: [BE] 이미지 전체 리스트 조회
    볼트 분류 결과 별 이미지 리스트 조회
    
    Closes #S08P31A301-394
    
    
    Former-commit-id: 0094f9c47240cd26443e9e1f158ba31ef9ed79b5

--- backend/semes/src/main/java/com/ssafy/semes/transition/model/service/TransitionService.java
+++ backend/semes/src/main/java/com/ssafy/semes/transition/model/service/TransitionService.java
@@ -1,8 +1,13 @@
 package com.ssafy.semes.transition.model.service;
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.ssafy.semes.image.model.ImageListResponseDto;
 
 @Service
 public interface TransitionService {
+	List<ImageListResponseDto> findAll();
 }
