33f81583f719c7b1213684f03f79e4665f82e748
안태윤 <cfdw011@gmail.com>
2023-04-27 16:56:09 +0900

    :sparkles: [BE] 볼트 이미지 삭제 API
    볼트 이미지 삭제 요청시 ARCHIVE 하위 폴더로 이동
    ARCHIVE 폴더 초기화 코드 추가
    
    Closes #S08P31A301-395
    
    
    Former-commit-id: 34bf6b7aea8441bc4bf7b6b3689e26b22d8970bd

--- backend/semes/src/main/java/com/ssafy/semes/SemesApplication.java
+++ backend/semes/src/main/java/com/ssafy/semes/SemesApplication.java
@@ -1,15 +1,24 @@
 package com.ssafy.semes;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import com.ssafy.semes.util.FileUtil;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAsync
 public class SemesApplication {
 	public static void main(String[] args) {
+		try {
+			FileUtil.init();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 		SpringApplication.run(SemesApplication.class, args);
 	}
 }
