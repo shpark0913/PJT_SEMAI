1b3da5077140b4462432704aa9ef284c79ce02b4
shpark0913 <shpark097754@gmail.com>
2023-04-27 13:34:34 +0900

    :sparkles: [FE] 로그인 기능 구현
    - 경로만 맞게 수정하면 정상적으로 기능
    
    closes #S08P31A301-332
    
    
    Former-commit-id: 09425989697f9225633975c84f1eb9e5996782ac

--- frontend/src/components/LoginPage/LoginInput.js
+++ frontend/src/components/LoginPage/LoginInput.js
@@ -1,19 +1,53 @@
 import "./LoginInput.css";
 
-import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
 function LoginInput() {
+  const [userId, setUserId] = useState("");
+  const [userPwd, setUserPwd] = useState("");
+
+  const onSubmitHandler = event => {
+    event.preventDefault();
+
+    let body = {
+      userId: userId,
+      userPwd: userPwd,
+    };
+
+    axios
+      .post("https://i8a108.p.ssafy.io/api/users/", body)
+      .then(response => {
+        // 로그인 성공 시 코드 작성(대시보드 이동, 토큰 저장)
+        console.log(response);
+      })
+      .catch(error => {
+        // 로그인 실패 시 코드 작성(Id, Pwd 초기화 & 경고 문구)
+        console.log(error);
+      });
+  };
+
+  const onIdHandler = event => {
+    setUserId(event.target.value);
+  };
+
+  const onPwdHandler = event => {
+    setUserPwd(event.target.value);
+  };
+
   return (
     <div class="login">
-      <form method="post">
+      <form onSubmit={onSubmitHandler}>
         <input
           type="text"
           name="u"
           placeholder="Username"
           required="required"
           autoComplete="off"
-          style={{ backgroundColor: "white" }}
           id="loginInput"
+          value={userId}
+          onChange={onIdHandler}
+          style={{ backgroundColor: "white" }}
         />
         <input
           type="password"
@@ -21,8 +55,10 @@ function LoginInput() {
           placeholder="Password"
           required="required"
           autoComplete="off"
-          style={{ backgroundColor: "white" }}
           id="loginInput"
+          value={userPwd}
+          onChange={onPwdHandler}
+          style={{ backgroundColor: "white" }}
         />
         <button type="submit" class="btn btn-primary btn-block btn-large">
           진단 시스템 로그인
