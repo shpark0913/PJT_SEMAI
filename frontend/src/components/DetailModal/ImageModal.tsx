0e9a1da7e01338dbed73ffa0b7773d15f63c9e48
gyur1kim <qri1114@gmail.com>
2023-04-28 16:32:17 +0900

    :lipstick: [FE] 볼트 이미지 상세보기 모달 CSS 완성
    -
    
    closes #S08P31A301-323
    
    
    Former-commit-id: 2696770d0bfb555a03231224683348c91b5c5ab2

--- frontend/src/components/DetailModal/ImageModal.tsx
+++ frontend/src/components/DetailModal/ImageModal.tsx
@@ -3,18 +3,42 @@ import {CloseButton, Modal, ModalBackground, ModalImageContainer} from "./ModalC
 import CloseIcon from '@mui/icons-material/Close';
 import Title from "../Title";
 import {ImageModalProps} from "../../_utils/Types";
+import styled from "styled-components";
 
+const CloseDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ImageDiv = styled.div<{ imageUrl: string | undefined }>`
+  flex: 1;
+  background-image: url(${props => props.imageUrl});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+const ButtonDiv = styled.div`
+  margin: 20px 0 0 0;
+  
+  display: flex;
+  justify-content: center;
+  & > button {
+    margin-right: 15px;
+    &:last-of-type {
+      margin-right: 0;
+    }
+  }
+`;
 function ImageModal({detailInfo, handleModalClose}: ImageModalProps) {
   return (
     <Modal>
       <ModalBackground />
       <ModalImageContainer>
-        <div style={{display: "flex", justifyContent: "right"}}>
+        <CloseDiv>
+          <Title title="볼트 이미지 상세보기" />
           <CloseButton onClick={handleModalClose}><CloseIcon sx={{height: "35px", width: "35px"}} /></CloseButton>
-        </div>
-        <Title title="볼트 이미지 상세보기" />
-        <div style={{flex: "1", backgroundImage: `url(${detailInfo.imageUrl})`, backgroundSize: "contain", backgroundRepeat: "no-repeat"}} />
-        <div style={{margin: "20px 0 0 0"}}>{detailInfo.buttons}</div>
+        </CloseDiv>
+        <ImageDiv imageUrl={detailInfo.imageUrl}/>
+        <ButtonDiv>{detailInfo.buttons}</ButtonDiv>
       </ModalImageContainer>
     </Modal>
   );
