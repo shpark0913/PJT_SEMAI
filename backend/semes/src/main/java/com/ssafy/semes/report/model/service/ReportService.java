6929b53edd6e64269c32f9a138a6656a39d9c23f
abin7989 <abin7989@gmail.com>
2023-04-28 13:42:52 +0900

    :sparkles: [BE] JPA N+1 해결 S08P31A301-310
    Former-commit-id: c0a653b2a24f01c69de4a12942074bb9e7e3f2db

--- backend/semes/src/main/java/com/ssafy/semes/report/model/service/ReportService.java
+++ backend/semes/src/main/java/com/ssafy/semes/report/model/service/ReportService.java
@@ -5,8 +5,10 @@ import com.ssafy.semes.report.model.ReportListResponseDto;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public interface ReportService {
-    List<ReportListResponseDto> findReport(QuestionDto dto) throws Exception;
+    Map<String, Object> findReport(QuestionDto dto) throws Exception;
     ReportListResponseDto findReportDetail(long wheelChcekId) throws Exception;
 }
