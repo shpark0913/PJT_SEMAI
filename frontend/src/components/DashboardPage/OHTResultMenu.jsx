a832f6b7db848db8128970b95fc0680e58051535
shpark0913 <shpark097754@gmail.com>
2023-04-28 14:10:37 +0900

    :sparkles: [FE] Dashboard API 연동
    - OHT 현재 검사 연동 완료
      - 하단 상세내역에서 조회할 때는 추가 구현해야 함
    - padstart 적용
    - SSE 데이터 수신 확인
    
    
    Former-commit-id: b6acb3b25d4410a8fe16e65cce3a9dee66fce51e

---
+++ frontend/src/components/DashboardPage/OHTResultMenu.jsx
@@ -0,0 +1,81 @@
+import { TBody, TD, TH, THeadMain, TR, Table } from "../TableComponents";
+import { useEffect, useState } from "react";
+
+import { EventSourcePolyfill } from "event-source-polyfill";
+import axios from "axios";
+
+function OHTResultMenu() {
+  const [dashboardData, setDashboardData] = useState([]);
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+  useEffect(() => {
+    const sse = new EventSourcePolyfill(`${BASE_URL}dashboard`, {
+      headers: {
+        accesstoken:
+          "eyJ0eXAiOiJKV1QiLCJyZWdEYXRlIjoxNjgyNDgxODQ0NjkxLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2ODM3Nzc4NDQsInN1YiI6ImFjY2Vzcy10b2tlbiIsInJvbGUiOiJBRE1JTiJ9.DsMQURAqUMNNA-aU7SoSLFL19FEtQkutnp5b3HsMX0k",
+      },
+    });
+
+    sse.addEventListener("dashboard", event => {
+      setDashboardData(JSON.parse(event.data));
+    });
+  }, []);
+
+  console.log("데이터", dashboardData);
+
+  return (
+    <Table>
+      <THeadMain style={{ borderTop: "solid 2px var(--emphasize-color)" }}>
+        <TR>
+          <TH>날짜</TH>
+          <TH>시간</TH>
+          <TH>호기 ID</TH>
+          <TH>판정 결과</TH>
+          <TH>&nbsp; FL &nbsp;</TH>
+          <TH>&nbsp; FR &nbsp;</TH>
+          <TH>&nbsp; RL &nbsp;</TH>
+          <TH>&nbsp; RR &nbsp;</TH>
+        </TR>
+      </THeadMain>
+      <TBody>
+        {dashboardData.map(item => {
+          let checkResult, data;
+          axios
+            .get(`${BASE_URL}dashboard/main/${item.ohtCheckId}`, {
+              headers: {
+                accesstoken:
+                  "eyJ0eXAiOiJKV1QiLCJyZWdEYXRlIjoxNjgyNDgxODQ0NjkxLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2ODM3Nzc4NDQsInN1YiI6ImFjY2Vzcy10b2tlbiIsInJvbGUiOiJBRE1JTiJ9.DsMQURAqUMNNA-aU7SoSLFL19FEtQkutnp5b3HsMX0k",
+              },
+            })
+            .then(response => {
+              checkResult = response.data.data;
+              console.log("바퀴 1개당 4개의 object 있어야", checkResult);
+            });
+          return (
+            <TR>
+              <TD></TD>
+              <TD></TD>
+              <TD></TD>
+              <TD></TD>
+              <TD></TD>
+              <TD></TD>
+              <TD></TD>
+              <TD></TD>
+            </TR>
+          );
+        })}
+        <TR>
+          <TD>2023-04-20</TD>
+          <TD>13:04:45</TD>
+          <TD>V30001-FL-1681704285</TD>
+          <TD>정상</TD>
+          <TD>-</TD>
+          <TD>-</TD>
+          <TD>1</TD>
+          <TD>3</TD>
+        </TR>
+      </TBody>
+    </Table>
+  );
+}
+
+export default OHTResultMenu;
