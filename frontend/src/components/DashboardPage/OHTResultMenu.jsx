bb89267f6804983d65d00caf219696fba8c3fa43
shpark0913 <shpark097754@gmail.com>
2023-05-01 13:19:33 +0900

    :sparkles: [FE] OHT 휠 검사 현황 마무리
    - 최신 검사가 상단으로 위치하게 수정
    - 전체, 에러 클릭 시 해당 항목들만 조회 가능
    
    closes #S08P31A301-434
    
    
    Former-commit-id: fb06c58a92564eab538b2eb5ef6390add13db5ca

--- frontend/src/components/DashboardPage/OHTResultMenu.jsx
+++ frontend/src/components/DashboardPage/OHTResultMenu.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { EventSourcePolyfill } from "event-source-polyfill";
 import { store } from "../../_store/store";
 
-function OHTResultMenu() {
+function OHTResultMenu(props) {
   const [dashboardData, setDashboardData] = useState([]);
   const BASE_URL = process.env.REACT_APP_BASE_URL;
 
@@ -34,33 +34,68 @@ function OHTResultMenu() {
           <TH>&nbsp; RR &nbsp;</TH>
         </TR>
       </THeadMain>
-      <TBody>
-        {dashboardData.reverse().map((item, idx) => {
-          return (
-            <TR key={idx} NG={item.flCount + item.frCount + item.rlCount + item.rrCount}>
-              <TD>
-                {item.ohtCheckStartDatetime[0]}-
-                {String(item.ohtCheckStartDatetime[1]).padStart(2, "0")}-
-                {String(item.ohtCheckStartDatetime[2]).padStart(2, "0")}
-              </TD>
-              <TD>
-                {" "}
-                {String(item.ohtCheckStartDatetime[3]).padStart(2, "0")}:
-                {String(item.ohtCheckStartDatetime[4]).padStart(2, "0")}:
-                {String(item.ohtCheckStartDatetime[5]).padStart(2, "0")}
-              </TD>
-              <TD>{item.ohtId}</TD>
-              <TD>
-                {item.flCount + item.frCount + item.rlCount + item.rrCount === 0 ? "정상" : "NG"}
-              </TD>
-              <TD>{item.flCount ? item.flCount : "-"}</TD>
-              <TD>{item.frCount ? item.frCount : "-"}</TD>
-              <TD>{item.rlCount ? item.rlCount : "-"}</TD>
-              <TD>{item.rrCount ? item.rrCount : "-"}</TD>
-            </TR>
-          );
-        })}
-      </TBody>
+      {props.isActive ? (
+        <TBody>
+          {dashboardData.map((item, idx) => {
+            return (
+              <TR key={idx} NG={item.flCount + item.frCount + item.rlCount + item.rrCount}>
+                <TD>
+                  {item.ohtCheckStartDatetime[0]}-
+                  {String(item.ohtCheckStartDatetime[1]).padStart(2, "0")}-
+                  {String(item.ohtCheckStartDatetime[2]).padStart(2, "0")}
+                </TD>
+                <TD>
+                  {" "}
+                  {String(item.ohtCheckStartDatetime[3]).padStart(2, "0")}:
+                  {String(item.ohtCheckStartDatetime[4]).padStart(2, "0")}:
+                  {String(item.ohtCheckStartDatetime[5]).padStart(2, "0")}
+                </TD>
+                <TD>{item.ohtId}</TD>
+                <TD>
+                  {item.flCount + item.frCount + item.rlCount + item.rrCount === 0 ? "정상" : "NG"}
+                </TD>
+                <TD>{item.flCount ? item.flCount : "-"}</TD>
+                <TD>{item.frCount ? item.frCount : "-"}</TD>
+                <TD>{item.rlCount ? item.rlCount : "-"}</TD>
+                <TD>{item.rrCount ? item.rrCount : "-"}</TD>
+              </TR>
+            );
+          })}
+        </TBody>
+      ) : (
+        <TBody>
+          {dashboardData.map((item, idx) => {
+            if (item.flCount + item.frCount + item.rlCount + item.rrCount !== 0) {
+              return (
+                <TR key={idx} NG={item.flCount + item.frCount + item.rlCount + item.rrCount}>
+                  <TD>
+                    {item.ohtCheckStartDatetime[0]}-
+                    {String(item.ohtCheckStartDatetime[1]).padStart(2, "0")}-
+                    {String(item.ohtCheckStartDatetime[2]).padStart(2, "0")}
+                  </TD>
+                  <TD>
+                    {" "}
+                    {String(item.ohtCheckStartDatetime[3]).padStart(2, "0")}:
+                    {String(item.ohtCheckStartDatetime[4]).padStart(2, "0")}:
+                    {String(item.ohtCheckStartDatetime[5]).padStart(2, "0")}
+                  </TD>
+                  <TD>{item.ohtId}</TD>
+                  <TD>
+                    {item.flCount + item.frCount + item.rlCount + item.rrCount === 0
+                      ? "정상"
+                      : "NG"}
+                  </TD>
+                  <TD>{item.flCount ? item.flCount : "-"}</TD>
+                  <TD>{item.frCount ? item.frCount : "-"}</TD>
+                  <TD>{item.rlCount ? item.rlCount : "-"}</TD>
+                  <TD>{item.rrCount ? item.rrCount : "-"}</TD>
+                </TR>
+              );
+            }
+            return null;
+          })}
+        </TBody>
+      )}
     </Table>
   );
 }
