42eaea62976166b5eac7239e2f7712c4fca4ab21
안태윤 <cfdw011@gmail.com>
2023-05-01 15:43:10 +0900

    :feature: [BE] 전이학습 이미지 폴더 초기화 코드
    폴더 초기화시 OS 별로 분기
    
    Closes #S08P31A301-400
    
    
    Former-commit-id: 229be982cbae8397e3da4d6fec55525dc18113f7

--- backend/semes/src/main/java/com/ssafy/semes/util/FileUtil.java
+++ backend/semes/src/main/java/com/ssafy/semes/util/FileUtil.java
@@ -15,8 +15,9 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class FileUtil{
-	static String ROOT = new File(new File(System.getProperty("user.dir")).getParent()).getParent();
-	static public void init(String baseDir) throws IOException {
+	static String pwd = System.getProperty("user.dir");
+	static String ROOT = pwd.equals(File.separator) ? new File(pwd).getPath() : new File(new File(pwd).getParent()).getParent();
+	static public void init(String baseDir) {
 		mkdir(getBasePath(baseDir));
 		Directory[] subDirs = Directory.getBoltDirectories();
 		for (Directory dir:
