4b745da3393430a07fdfe81df9e87df801759ac0
gyur1kim <qri1114@gmail.com>
2023-05-02 14:14:26 +0900

    :recycle: [FE] useDate 수정
    - date 부분 함수 수정
      - padStart를 이용해 수정
      - 공통된 함수 합침
    
    closes #S08P31A301-399
    
    
    Former-commit-id: 41879af543155615d6d62abdfe64781354b4052e

--- frontend/src/_hooks/useDate.tsx
+++ frontend/src/_hooks/useDate.tsx
@@ -1,36 +1,38 @@
 import {useCallback} from "react";
 
 function useDate() {
-  /** 날짜, 시간 등... 2자리로 만들어줌 */
+  /** 숫자가 input으로 들어올 때, 2자리 이상으로 만들어줌 */
   const datePadStart = useCallback((date: number) => {
     return String(date).padStart(2, "0");
   }, []);
 
-  const createWheelCheckId = useCallback((arg: number[]) => {
-    return `${arg[0]}${datePadStart(arg[1])}${datePadStart(arg[2])}${datePadStart(arg[3])}${datePadStart(arg[4])}${datePadStart(arg[5])}`
+  /** 년월일시분초 합치는 함수 */
+  const wheelCheckId = useCallback((arg: number[]) => {
+    let idList = arg.map(num => datePadStart(num));
+    return idList.join('');
   }, [datePadStart]);
 
-  const timeFormat = useCallback((time: number) => {
-    return `${datePadStart(time)}:00`
+  /** 시:분:초 형식으로 변환 */
+  const timeFormat = useCallback((arg: number[]) => {
+    let idList = arg.map(num => datePadStart(num));
+    return idList.join(':');
   }, [datePadStart]);
 
-  const timeSecFormat = useCallback((arg: number[]) => {
-    return `${datePadStart(arg[0])}:${datePadStart(arg[1])}:${datePadStart(arg[2])}`
-  }, [datePadStart]);
-
+  /** 년-월-일 형식으로 변환 */
   const dateFormat = useCallback((arg: number[]) => {
-    return `${String(arg[0])}-${datePadStart(arg[1])}-${datePadStart(arg[2])}`
+    let idList = arg.map(num => datePadStart(num));
+    return idList.join('-');
   }, [datePadStart]);
 
-  /** 그냥 형식을 date 형식으로 만들어줌. */
-  const timestampFormat = useCallback((timestamp=new Date()) => {
+  /** 오늘 날짜 가져오기. */
+  const todayFormat = useCallback((timestamp=new Date()) => {
     let year = String(timestamp.getFullYear());
     let month = datePadStart(timestamp.getMonth() + 1);
     let day = datePadStart(timestamp.getDate()).padStart(2, "0");
     return `${year}-${month}-${day}`;
   }, [datePadStart]);
 
-  return { dateFormat, timeSecFormat, timestampFormat, timeFormat, createWheelCheckId }
+  return { dateFormat, todayFormat, timeFormat, wheelCheckId }
 }
 
 export default useDate;
