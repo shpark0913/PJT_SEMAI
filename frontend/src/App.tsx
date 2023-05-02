f385ab7a9e0106c98bc6bbec51a9f6232fdb95f5
shpark0913 <shpark097754@gmail.com>
2023-05-02 13:32:50 +0900

    :art: [FE] 비동기 함수를 통해 대시보드 api 연동
    - 비동기 함수를 통해 SSE 데이터로 대시보드 페이지 첫 렌더링
    
    closes #S08P31A301-433
    
    
    Former-commit-id: ef5456074946baf2cce62b0c748cc92efd512303

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { BoltImageListsLoader, ReportListsLoader } from "./_utils/Loader";
 import DashboardPage, { loader as DashboardLoader } from "./pages/DashboardPage";
-import DashboardPage2, { loader as DashboardLoader2 } from "./pages/DashboardPage2";
-
+import DashboardPageDetail, {
+  loader as DashboardPageDetailLoader,
+} from "./pages/DashboardPageDetail";
+import { ReportListsAction, TransferTestAction } from "./_utils/Action";
 import {
   Route,
   RouterProvider,
@@ -12,13 +14,10 @@ import {
 import GlobalStyle from "./components/globalStyle";
 import LayoutPage from "./pages/LayoutPage";
 import LoginPage from "./pages/LoginPage";
+import React from "react";
 import ReportPage from "./pages/ReportPage";
 import TransferPage from "./pages/TransferPage";
 import { store } from "./_store/store";
-
-import { BoltImageListsLoader, ReportListsLoader } from "./_utils/Loader";
-import { ReportListsAction, TransferTestAction } from "./_utils/Action";
-
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,12 +26,27 @@ const router = createBrowserRouter(
       <Route path="/" element={<LayoutPage />}>
         <Route index element={<DashboardPage />} loader={DashboardLoader} />
 
-        <Route path="report" element={<ReportPage />} id="reportLists" loader={ReportListsLoader} action={ReportListsAction} />
+        <Route
+          path="report"
+          element={<ReportPage />}
+          id="reportLists"
+          loader={ReportListsLoader}
+          action={ReportListsAction}
+        />
 
-        <Route path=":checkId" element={<DashboardPage2 />} loader={DashboardLoader2} />
+        <Route
+          path=":checkId"
+          element={<DashboardPageDetail />}
+          loader={DashboardPageDetailLoader}
+        />
 
         <Route path="report/:id" />
-        <Route path="transfer" element={<TransferPage />} loader={BoltImageListsLoader} action={TransferTestAction} />
+        <Route
+          path="transfer"
+          element={<TransferPage />}
+          loader={BoltImageListsLoader}
+          action={TransferTestAction}
+        />
       </Route>
     </Route>,
   ),
