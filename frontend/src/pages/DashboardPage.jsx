4152461f4a22cafb0613fbfbfd1d0c3e280e8129
shpark0913 <shpark097754@gmail.com>
2023-05-04 11:03:24 +0900

    :recycle: [FE] Dashboard 코드 리팩토링
    closes #S08P31A301-440
    
    
    Former-commit-id: 639148c6df90d52ae1d12286161c16b4d10c8599

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,11 +1,15 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+
 import Axios from "../_utils/Axios";
 import { EventSourcePolyfill } from "event-source-polyfill";
 import OHTAllResult from "../components/DashboardPage/OHTAllResult";
 import OHTCheck from "../components/DashboardPage/OHTCheck";
 import OHTResult from "../components/DashboardPage/OHTResult";
 import OHTTransition from "../components/DashboardPage/OHTTransition";
+import { setCheckId } from "../_store/slices/dashboardSlice";
 import styled from "styled-components";
-import { useLoaderData } from "react-router";
+import { useLoaderData } from "react-router-dom";
 
 export async function loader() {
   const BASE_URL = process.env.REACT_APP_BASE_URL;
@@ -23,7 +27,7 @@ export async function loader() {
     });
     const response = await new Promise(resolve => {
       sse.addEventListener("dashboard", event => {
-        console.log("SSE 작동");
+        console.log("SSE_dashboard 작동");
         dashboardData = JSON.parse(event.data);
         resolve(dashboardData);
       });
@@ -31,15 +35,12 @@ export async function loader() {
     return response;
   }
 
-  async function getData() {
-    const data = await fetchData();
-    const response = await Axios.get(`${BASE_URL}dashboard/main/${data[0].ohtCheckId}`);
-    return [response.data.data];
-  }
+  let newOHTCheckId;
+  fetchData().then(response => {
+    newOHTCheckId = response[0].ohtCheckId;
+  });
 
-  const result = await getData();
-  const wheelData = result[0];
-  return [wheelData];
+  return [newOHTCheckId];
 }
 
 const MainGrid = styled.section`
@@ -51,10 +52,43 @@ const MainGrid = styled.section`
 `;
 
 function DashboardPage() {
-  const wheelData = useLoaderData();
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+  const dispatch = useDispatch();
+
+  console.log(
+    "넘어오나?",
+    useSelector(state => state.dashboard),
+  );
+
+  // SSE의 최상단 검사 ID (최신)
+  const newOHTCheckId = useLoaderData();
+  console.log("========newOHTCheckID========", newOHTCheckId);
+  dispatch(setCheckId(newOHTCheckId));
+
+  // 특정 ohtCheckId로 조회한 정보 (휠 4개의 상태)
+  const [wheelDataNew, setWheelDataNew] = useState(null);
+
+  // store에 저장된 ohtCheckId를 ohtCheckId 변수에 저장
+  const ohtCheckId = useSelector(state => {
+    return state.dashboard.checkId;
+  });
+
+  useEffect(() => {
+    async function fetchWheelData() {
+      try {
+        const response = await Axios.get(`${BASE_URL}dashboard/main/${ohtCheckId}`);
+        const wheelData = response.data.data;
+        setWheelDataNew(wheelData);
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+    fetchWheelData();
+  }, [ohtCheckId]);
+
   return (
     <MainGrid>
-      <OHTResult data={wheelData[0]} />
+      {wheelDataNew ? <OHTResult data={wheelDataNew} /> : null}
       <OHTCheck />
       <OHTTransition />
       <OHTAllResult />
