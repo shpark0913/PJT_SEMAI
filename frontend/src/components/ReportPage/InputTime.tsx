6f122882808c1084a005a62a00f4046f60eb032b
gyur1kim <qri1114@gmail.com>
2023-05-04 17:53:02 +0900

    :recycle: [FE] query의 search params를 하위 컴포넌트에 props로 넘김
    - QueryType 셍성
    
    closes #S08P31A301-447
    
    
    Former-commit-id: a72020d6fe84d1e0fe37dd951b70a07d2f09127f

--- frontend/src/components/ReportPage/InputTime.tsx
+++ frontend/src/components/ReportPage/InputTime.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Label} from "./FilterComponents";
 import useDate from "../../_hooks/useDate";
 import {useAppSelector} from "../../_hooks/hooks";
 
 
-function InputTime({startDate, endDate, time}: {
+function InputTime({startDate, endDate, query}: {
   startDate: string,
   endDate: string,
-  time: string,
+  query: URLSearchParams,
 }) {
+  let [time, setTime] = useState<string>(query.get('time') || "ALL");
   let {timeFormat} = useDate();
   const timeInput = []
   for(let i=0; i<24; i++) {
-    timeInput.push(<option key={`time-key-${i + 1}`} value={i}>{timeFormat([i, 0])}</option>)
+    timeInput.push(<option key={`time-key-${i + 1}`} selected={startDate !== endDate} value={i}>{timeFormat([i, 0])}</option>)
   }
   let theme= useAppSelector(state => state.theme.theme)
+
 
   return (
   <Label theme={theme}> 검사 시간
