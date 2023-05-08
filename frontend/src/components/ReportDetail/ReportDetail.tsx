ada3c415a0e3fb33e8d5ff5446e614889738d4dc
gyur1kim <qri1114@gmail.com>
2023-05-08 16:58:18 +0900

    :sparkles: [FE] report table 스크롤 기능
    - table을 감싼 div태그에 높이를 주고, 높이보다 커지면 스크롤되게 설정
    - thead와 tfoot에 sticky 속성 지정, 스크롤시에도 움직이지 않음
    - border-collapse: separate와 border-spacing을 이용해 border가 움직이지 않게 고정
    
    closes #S08P31A301-495
    
    
    Former-commit-id: 3e0757e152547c4a07d1d5bd6675c0bbdc65e936

--- frontend/src/components/ReportDetail/ReportDetail.tsx
+++ frontend/src/components/ReportDetail/ReportDetail.tsx
@@ -7,12 +7,10 @@ import DetailTable from "../Modal/DetailTable";
 
 const ReportDetailContainer = styled.div`
   background-color: var(--background-color);
-  //box-shadow: -10px 0px 10px -10px rgba(33, 35, 38, 0.1);
-  //box-shadow: rgba(33, 35, 38, 0.1) -10px 0px 10px -10px;
-  //flex-shrink: 1;
   width: 100%;
   height: 100%;
   padding-left: 30px;
+  margin-left: 30px;
   transition: width 1000ms ease, transform 1000ms ease, box-shadow 1000ms ease;
   
   &.open{
--- frontend/src/components/TableComponents.tsx
+++ frontend/src/components/TableComponents.tsx
@@ -7,23 +7,24 @@ type TRType = {
 const TableContainer = styled.div`
   margin-bottom: 10px;
   width: 100%;
-  
+  height: 100%;
   min-width: 800px;
   overflow-y: auto;
-  flex-grow: 1;
-  flex-shrink: 0;
+  //flex-grow: 1;
+  //flex-shrink: 1;
 `;
 
 const Table = styled.table`
   width: 100%;
-  border-collapse: collapse;
-
+  border-collapse: separate; /* Don't collapse */
+  border-spacing: 0;
+  
+  //border-collapse: separate;
   &.detail {
     border-left: 3px solid var(--emphasize-color);
     & tr:last-of-type th {
       border-bottom: none;
     }
-
     & th {
       width: 25%;
     }
@@ -35,8 +36,15 @@ const Table = styled.table`
 `;
 
 const THead = styled.thead`
-  border-top: 3px solid var(--emphasize-color);
-  border-bottom: 1px solid var(--emphasize-color);
+  //border-top: 3px solid var(--emphasize-color);
+  //border-bottom: 1px solid var(--emphasize-color);
+  position: sticky;
+  top: 0;
+  
+  & > tr:first-child > th {
+    border-top: 3px solid var(--emphasize-color);
+    border-bottom: 1px solid var(--emphasize-color);
+  }
 `;
 
 const THeadMain = styled.thead`
@@ -81,13 +89,24 @@ const TBody = styled.tbody`
   & > tr:last-child td {
     border-bottom: none;
   }
+  & > tr:last-child th {
+    border-bottom: none;
+  }
 `;
 
 const TFoot = styled.tfoot`
   text-align: center;
-  border-top: 1px solid var(--emphasize-color);
-  border-bottom: 3px solid var(--emphasize-color);
+  //border-top: 1px solid var(--emphasize-color);
+  //border-bottom: 3px solid var(--emphasize-color);
   background-color: var(--background-dark-color);
+
+  position: sticky;
+  bottom: 0;
+
+  & > tr:first-child > td {
+    border-top: 1px solid var(--emphasize-color);
+    border-bottom: 3px solid var(--emphasize-color);
+  }
 `;
 
 const TR = styled.tr<TRType>`
