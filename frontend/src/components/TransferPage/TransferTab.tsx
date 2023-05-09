8a0ad2c8de65fc4dcecb0a4d2ac00fbac4fa6231
gyur1kim <qri1114@gmail.com>
2023-05-09 15:13:36 +0900

    :recycle: [FE] menu와 본문 컴포넌트화
    - 컴포넌트로 나누어 따로 관리하기
    
    closes #S08P31A301-517
    
    
    Former-commit-id: 00a7b981c68fa489f39fe87f1c3d495f83273ee3

--- frontend/src/components/TransferPage/TransferTab.tsx
+++ frontend/src/components/TransferPage/TransferTab.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TransferImage, TransferImageGrid, TransferImageGridContainer } from "./TransferImageComponents";
+import { TransferBoltImage, TransferImageGrid, TransferImageGridContainer } from "./TransferImageComponents";
 import {
   TransferContainer,
   TransferMenuContainer,
@@ -12,81 +12,33 @@ import {Form, useRouteLoaderData} from "react-router-dom";
 
 
 const TransferTab = () => {
-  const [tabIdx, setTabIdx] = useState(0);
-  const [isDetailOpen, setIsDetailOpen] = useState(false);
+  const [tabIndex, setTabIndex] = useState(0);
+
   let BoltImageLists = useRouteLoaderData("transfer") as TransferLoaderType[];
+  console.log(BoltImageLists)
 
   const TabMenuList = ['양호', '유실', '풀림', '모호'];
-  const ButtonList: JSX.Element[] = [<>
-    <Button>유실로 이동</Button>
-    <Button>풀림으로 이동</Button>
-    <SemesButton>학습으로 이동</SemesButton>
-    <RedButton>삭제하기</RedButton>
-  </>, <>
-    <Button>양호로 이동</Button>
-    <Button>풀림으로 이동</Button>
-    <SemesButton>학습으로 이동</SemesButton>
-    <RedButton>삭제하기</RedButton>
-  </>, <>
-    <Button>양호로 이동</Button>
-    <Button>유실로 이동</Button>
-    <SemesButton>학습으로 이동</SemesButton>
-    <RedButton>삭제하기</RedButton>
-  </>, <>
-    <Button>양호로 이동</Button>
-    <Button>유실로 이동</Button>
-    <Button>풀림으로 이동</Button>
-    <RedButton>삭제하기</RedButton>
-  </>];
 
-  const tabComponent = BoltImageLists.map((status) => {
-    return {
-      menu: TabMenuList[status.status],
-      content:
-        <> {
-        status.images.map((image) =>
-          <TransferImage key={`bolt_images-${image.fileId}`}><img src={image.imgUrl} alt="bolt" />
-            <div onClick={() => setIsDetailOpen(true) }>
-              {image.originName}
-            </div>
-          </TransferImage>
-        )
-        } </>,
-      counts: status.images.length,
-    }
-  })
+
+
 
   return (
     <TransferContainer>
 
       <TransferMenuContainer>
-        { tabComponent.map((tab, idx) =>
+        { TabMenuList.map((menu, idx) =>
           <li
             key={`transfer-tab-menu-${idx}`}
-            className={idx === tabIdx ? "isActive" : "" }
-            onClick={ () => setTabIdx(idx) }
+            className={idx === tabIndex ? "isActive" : "" }
+            onClick={ () => setTabIndex(idx) }
           >
-            {tab.menu} <span>{tab.counts}</span>
+            {TabMenuList[idx]}<span>{BoltImageLists[idx].images.length}</span>
           </li>
         ) }
       </TransferMenuContainer>
 
       <TransferImageContainer>
-        <Form style={{display: "flex", flexDirection: "column", height: "100%"}}>
-          <label>전체 선택 <input type="checkbox" /></label>
-          <TransferImagesDetailWrapper>
-            <TransferImageGridContainer className={isDetailOpen? "active" : ""}>
-              <TransferImageGrid className={isDetailOpen? "active" : ""}>{tabComponent[tabIdx].content}</TransferImageGrid>
-            </TransferImageGridContainer>
-            <TransferImageDetailContainer className={isDetailOpen? "active" : ""}>
-              이미지 미리보기 영역
-            </TransferImageDetailContainer>
-          </TransferImagesDetailWrapper>
-          <div>
-            { ButtonList[tabIdx] }
-            <div>현재 선택 : 1/100</div>
-          </div>
-        </Form>
+        <TransferBoltImage tabIndex={tabIndex} BoltImageLists={BoltImageLists} />
       </TransferImageContainer>
     </TransferContainer>
   );
