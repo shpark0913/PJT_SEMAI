aa05d45a31992d4694845ca5d78e3dd0c01d30b7
gyur1kim <qri1114@gmail.com>
2023-05-10 09:52:47 +0900

    :sparkles: [FE] 학습과 클래스별 이미지 분류
    - 서버에서 주는 값의 0번째 idx는 클래스별 분류, 1번째 idx는 학습용 데이터가 넘어온다
    
    closes #S08P31A301-519
    
    
    Former-commit-id: 63cf88048082f53c9970ddc908d4faa1c5e269e4

--- frontend/src/pages/TransferPage.tsx
+++ frontend/src/pages/TransferPage.tsx
@@ -8,6 +8,7 @@ import {
 import TransferBoltImages from "../components/TransferPage/TransferBoltImages";
 import {useLoaderData} from "react-router-dom";
 import {TransferLoaderType} from "../_utils/Types";
+import LearningBoltImages from "../components/TransferPage/LearningBoltImages";
 
 const TransferSection = styled.section`
   padding: 30px;
@@ -19,9 +20,9 @@ const TransferSection = styled.section`
 function TransferPage() {
   const [tabIndex, setTabIndex] = useState(0);
   const [isDetailOpen, setIsDetailOpen] = useState(false);
-  let BoltImageLists = useLoaderData() as TransferLoaderType[];
+  let BoltImageLists = useLoaderData() as TransferLoaderType[][];
   console.log(BoltImageLists);
-  const TabMenuList = ['양호', '유실', '풀림', '모호'];
+  const TabMenuList = ['양호', '유실', '파단', '학습'];
 
   return (
     <TransferSection>
@@ -37,13 +38,18 @@ function TransferPage() {
                 setIsDetailOpen(false);
               } }
             >
-              {TabMenuList[idx]}<span>{BoltImageLists[idx].images.length}</span>
+              {TabMenuList[idx]}<span>{idx < 3 ? BoltImageLists[0][idx].images.length : ""}</span>
             </li>
           ) }
         </TransferMenuContainer>
 
         <TransferImageContainer>
-          <TransferBoltImages tabIndex={tabIndex} BoltImageLists={BoltImageLists} isDetailOpen={isDetailOpen} setIsDetailOpen={setIsDetailOpen} />
+          { tabIndex < 3 ?
+            <TransferBoltImages tabIndex={tabIndex} BoltImageLists={BoltImageLists[0]} isDetailOpen={isDetailOpen} setIsDetailOpen={setIsDetailOpen} /> :
+            <LearningBoltImages tabIndex={tabIndex} BoltImageLists={BoltImageLists[1]} isDetailOpen={isDetailOpen} setIsDetailOpen={setIsDetailOpen} />
+          }
+
+
         </TransferImageContainer>
       </TransferContainer>
 
