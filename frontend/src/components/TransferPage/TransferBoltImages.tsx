700d73485a050658c07d9916fcfc30888e082cc5
gyur1kim <qri1114@gmail.com>
2023-05-10 14:27:48 +0900

    :sparkles: [FE] 볼트 이미지
    - BASE_URL로 이미지 요청 보냄
    
    closes #S08P31A301-520
    
    
    Former-commit-id: 274ba9dd803b3c9a1266f72e5dcc259e64892d4e

--- frontend/src/components/TransferPage/TransferBoltImages.tsx
+++ frontend/src/components/TransferPage/TransferBoltImages.tsx
@@ -14,7 +14,8 @@ function TransferBoltImages({tabIndex, BoltImageLists, isDetailOpen, setIsDetail
                                 isDetailOpen: boolean,
                                 setIsDetailOpen: (arg: boolean) => void }) {
 
-
+  const IMG_URL = process.env.REACT_APP_IMG_URL;
+  console.log(IMG_URL)
   const { TransferClass } = useTransferBoltImages()
   const ButtonLists: JSX.Element[] = [<>
     <Button onClick={() => TransferClass(0, 1, [3123, 246])}>유실로 이동</Button>
@@ -40,7 +41,8 @@ function TransferBoltImages({tabIndex, BoltImageLists, isDetailOpen, setIsDetail
   const BoltImageElement = BoltImageLists.map((status) =>
         <> {
           status.images.map((image) =>
-            <TransferBoltImage key={`bolt_images-${image.fileId}`}><img src={image.imgUrl} alt="bolt" />
+            <TransferBoltImage key={`bolt_images-${image.fileId}`}>
+              <img src={`${IMG_URL}/${image.imgUrl}`} alt="bolt" />
               <div onClick={() => {
                 setIsDetailOpen(true);
                 setDetailInfo({imgUrl: image.imgUrl, originName: image.originName, fileId: image.fileId})
@@ -63,11 +65,11 @@ function TransferBoltImages({tabIndex, BoltImageLists, isDetailOpen, setIsDetail
       <label>전체 선택 <input type="checkbox" /></label>
       <TransferImagesDetailWrapper>
         <TransferImageGridContainer className={isDetailOpen? "active" : ""}>
-          <TransferImageGrid className={isDetailOpen? "active" : ""}>{BoltImageElement[tabIndex]}</TransferImageGrid>
+          <TransferImageGrid className={isDetailOpen? "active open" : "open"}>{BoltImageElement[tabIndex]}</TransferImageGrid>
         </TransferImageGridContainer>
         <TransferImageDetailContainer className={isDetailOpen? "active" : ""}>
           <CloseButton onClick={() => setIsDetailOpen(false)}><KeyboardDoubleArrowRightIcon sx={{height: "35px", width: "35px"}} /></CloseButton>
-          <img src={detailInfo.imgUrl} alt="bolt detail"/>
+          <img src={`${IMG_URL}/${detailInfo.imgUrl}`} alt="bolt detail"/>
           <div>{detailInfo.originName}</div>
         </TransferImageDetailContainer>
       </TransferImagesDetailWrapper>
