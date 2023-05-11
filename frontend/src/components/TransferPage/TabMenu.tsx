700a3008a5e9fbccd05b7c840fd89808cc0a0fcb
gyur1kim <qri1114@gmail.com>
2023-05-11 17:32:45 +0900

    :recycle: [FE] TabMenu 리사이클
    - state로 관리되던 값을 redux에 저장
    
    closes #S08P31A301-513
    
    
    Former-commit-id: 94685d5facbd094b0ec989a9bb8c0520f304067f

--- frontend/src/components/TransferPage/TabMenu.tsx
+++ frontend/src/components/TransferPage/TabMenu.tsx
@@ -3,14 +3,10 @@ import {TabMenuLi, TransferMenuContainer, LengthSpan} from "./TabMenuComponents"
 import {useAppDispatch, useAppSelector} from "../../_hooks/hooks";
 import {setTabIndex, setIsDetailOpen} from "../../_store/slices/transferPageSlice";
 
-function TabMenu({imageLengthList}: {
-  imageLengthList: number[],
-}) {
+function TabMenu({imageLengthList}: { imageLengthList: number[] }) {
 
   const dispatch = useAppDispatch();
   const { tabIndex, tabMenuList } = useAppSelector(state => state.transferPage);
-
-  // const TabMenuList: string[] = useMemo(() => ['양호', '유실', '파단', '학습'], []);
 
   return (
     <TransferMenuContainer>
