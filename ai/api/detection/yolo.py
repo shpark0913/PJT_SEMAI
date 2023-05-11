267d5bc20532af1f3330c88e757c5881ec9f3330
YU DAVID <loveneverfails95@naver.com>
2023-05-11 17:16:50 +0900

    :recyle: [BE] 디텍션 모델 업데이트 - 최종 학습한 모델로 yolo 가중치 업데이트 - 전처리 함수 수정 -> cv2 제거 - detect.py export.py yolov5 기존 파일로 교체
    Former-commit-id: cced92238d9e3de30516a8f8fb9cbcc09375057d

--- ai/api/detection/yolo.py
+++ ai/api/detection/yolo.py
@@ -1,8 +1,8 @@
 import re
 import os
-import cv2
 import torch
 from PIL import Image
+from io import BytesIO
 
 NOW_DIR = os.getcwd()
 os.chdir('./detection')
@@ -13,19 +13,26 @@ TARGET_IMAGE_SIZE = 2048
 CROP_SIZE = (200, 0, 200, 0)
 IMAGE_ORIGIN_PATH = '../../../dataset/semes_bolt/WHEEL_ORIGIN/'
 MODEL_PATH = 'weights/'
+MODEL_NAME = 'yolo_0511.pt'
 NORMAL_BBOXES_PATH = '../../../dataset/semes_bolt/DETECTION_NORMAL/'
 PROBLEM_BBOXES_PATH = '../../../dataset/semes_bolt/DETECTION_PROBLEM/'
 REGEX = re.compile('.jpg|.png|.jpeg|.gif|.bmp|.JPG|.PNG|.JPEG|.GIF|.BMP')
 
+
+## yolo 모델
 def load_detection_model(model_name, model_path=MODEL_PATH):
     path = model_path + model_name
     return torch.hub.load('.', 'custom', path=path, source='local')
 
 ## 서버를 실행시킬 때 바로 model load ##
-model = load_detection_model('yolo_0501.pt')
-model.conf = 0.5
+model = load_detection_model(MODEL_NAME)
+# confidence를 높여서 더욱 정밀하게 추론하도록 만듦
+model.conf = 0.7
+# 하나의 bbox에 대해 여러 라벨을 할당하지 못하도록 만듦
+model.multi_label = False
 
-## 전처리 함수 ##
+
+## 전처리 함수
 def preprocess_image(image, image_size=TARGET_IMAGE_SIZE, crop_size=CROP_SIZE, interpolation=Image.LANCZOS):
     '''
     Parameters
@@ -33,7 +40,6 @@ def preprocess_image(image, image_size=TARGET_IMAGE_SIZE, crop_size=CROP_SIZE, i
     - image_size: (int) yolo input 이미지 사이즈 / default: 2048
     - crop_size: (list, tuple) "좌-상-우-하" 순서로 자를 길이 / default: (0, 0, 0, 0)
     - interpolation: (int) interpolation 방식 / default: Image.LANCZOS
-    - 
     '''
 
     # 이미지 crop -> 정사각형으로 만들기
@@ -42,9 +48,9 @@ def preprocess_image(image, image_size=TARGET_IMAGE_SIZE, crop_size=CROP_SIZE, i
 
     # resize
     resized = cropped.resize((image_size, image_size), interpolation)
-    resized_name = 'temp/resized.jpg'
-    resized.save(resized_name)
-    return cv2.imread(resized_name)
+    b = BytesIO()
+    resized.save(b, 'jpeg')
+    return Image.open(b)
 
 
 ## 디텍션 함수 ##
@@ -60,8 +66,6 @@ def detect_bolt(image_path, model=model):
     # image에 해당 휠 이미지 열기
     now_image = Image.open(IMAGE_ORIGIN_PATH + image_path)
     now_image = preprocess_image(now_image)
-    now_image = cv2.cvtColor(now_image, cv2.COLOR_BGR2RGB)
-    now_image = Image.fromarray(now_image)
 
     result = model(now_image, size=TARGET_IMAGE_SIZE)
 
