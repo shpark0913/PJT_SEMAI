f4e558fe5d5433086ff1d854c04378cd082e51d4
gyur1kim <qri1114@gmail.com>
2023-05-14 16:20:07 +0900

    :art: [FE] confirm 모달 띄우기
    - 모달 띄우기는 성공
    - 로직에 변화가 필요할 듯 하다.
    
    closes #S08P31A301-584
    
    
    Former-commit-id: 4d9b2d46d69fdd0922b988b42954a0d117b1fdbe

--- frontend/src/components/TransferPage/TransferButtons.tsx
+++ frontend/src/components/TransferPage/TransferButtons.tsx
@@ -2,40 +2,71 @@ import React, {useState} from "react";
 import {Button, RedButton, SemesButton} from "../ButtonComponents";
 import useTransferBoltImages from "../../_hooks/useTransferBoltImages";
 import {TransferBoltImageObject} from "../../_utils/Types";
+// import useConfirmModal from "../../_hooks/useConfirmModal";
+// import ConfirmModal from "./ConfirmModal";
 
 
 function TransferButtons() {
+  // const { setIsConfirmModalOpen } = useConfirmModal();
+  let [isConfirmModalOpen, setIsConfirmModalOpen] = useState<boolean>(false);
   const { TransferClass, TransferLearning, DeleteImages } = useTransferBoltImages();
-  let [isConfirmOpen, setIsConfirmOpen] = useState<boolean>(false);
-  let TransferValue = ['양호로 이동', '유실로 이동', '파단으로 이동', '학습으로 이동', '삭제하기', ];
-  // 제출 버튼
-  // 0: 양호 , 1: 유실, 2: 파단
+  // let [isConfirmOpen, setIsConfirmOpen] = useState<boolean>(false);
 
-  const ConfirmModal = isConfirmOpen ?
-    <div>
-      <div style={{position: "absolute", top: "100px", left: "100px"}}>
-        안녕하세요
-      </div>
-    </div> :
-    <></>
+  let [type, setType] = useState<{preType: number, nextType: number}>({preType: 0, nextType: 1});
+  let TransferValue = ['양호로 이동', '유실로 이동', '파단으로 이동', '학습으로 이동', '삭제하기'];
 
-  const OpenConfirmModal = (nextType: number) => {
-    switch (nextType) {
-      case 0:
-        return <Button onClick={ () => setIsConfirmOpen(true) }>{ TransferValue[0] }</Button>
-      case 1:
-        return <Button onClick={ () => setIsConfirmOpen(true) }>{ TransferValue[1] }</Button>
-      case 2:
-        return <Button onClick={ () => setIsConfirmOpen(true) }>{ TransferValue[2] }</Button>
-      case 3:
-        return <SemesButton onClick={ () => setIsConfirmOpen(true) }>{ TransferValue[3] }</SemesButton>
-      case 4:
-        return <RedButton onClick={ () => setIsConfirmOpen(true) }>{ TransferValue[4] }</RedButton>
+  // type에 대해서
+  // 0: 양호, 1: 유실, 2: 파단, 3: 학습, 4: 삭제
+
+  const handleOpenConfirmModal = (preType: number, nextType: number) => {
+    setIsConfirmModalOpen(true);
+    setType({
+      preType: preType,
+      nextType: nextType
+    });
+  }
+
+  /** 모달창 띄우는 버튼 */
+  const ConfirmTransferClassButton = (preType: number): JSX.Element[] => {
+    if (preType < 3 ) {      // 클래스 탭인 경우 -> 클래스 이동, 학습 이동, 삭제 기능
+      return TransferValue.map((val, idx) => {
+        if (idx === preType) return <></>;
+        if (idx < 3) return <Button onClick={() => {
+          handleOpenConfirmModal(preType, idx);
+        }}> {val} </Button>;
+        else if (idx === 3) return <SemesButton onClick={() => {
+          handleOpenConfirmModal(preType, idx);
+        }}> {val} </SemesButton>;
+        else return <RedButton onClick={() => {
+            handleOpenConfirmModal(preType, idx);
+          }}> {val} </RedButton>;
+      });
+    }
+    else {     // 학습하기 탭인 경우 -> 학습 버튼과 삭제 버튼만..
+      return [
+        <SemesButton onClick={() => handleOpenConfirmModal(preType, 3)}> '학습하기' </SemesButton>,
+        <RedButton onClick={() => {handleOpenConfirmModal(preType, 4)}}> '삭제하기' </RedButton>
+      ]
     }
   }
-  const CancelButton = () => {
-    return <Button onClick={ () => setIsConfirmOpen(false) }>취소하기</Button>
+  const CancelConfirmModalButton = () => {
+    return <Button onClick={ () => setIsConfirmModalOpen(false) }>취소하기</Button>
   }
+
+  // const OpenConfirmModal = (nextType: number) => {
+  //   switch (nextType) {
+  //     case 0:
+  //       return <Button onClick={ () => setIsConfirmOpen(true) }>{ TransferValue[0] }</Button>
+  //     case 1:
+  //       return <Button onClick={ () => setIsConfirmOpen(true) }>{ TransferValue[1] }</Button>
+  //     case 2:
+  //       return <Button onClick={ () => setIsConfirmOpen(true) }>{ TransferValue[2] }</Button>
+  //     case 3:
+  //       return <SemesButton onClick={ () => setIsConfirmOpen(true) }>{ TransferValue[3] }</SemesButton>
+  //     case 4:
+  //       return <RedButton onClick={ () => setIsConfirmOpen(true) }>{ TransferValue[4] }</RedButton>
+  //   }
+  // }
 
   /** 다른 클래스로 이동하기  */
   const TransferClassButton = (preType: number, nextType: number, fileIds: number[]) => {
@@ -64,7 +95,7 @@ function TransferButtons() {
       });
     }}>{TransferValue[4]}</RedButton>
   }
-  return { ConfirmModal, OpenConfirmModal, CancelButton, TransferClassButton, TransferLearningButton, DeleteImagesButton }
+  return { isConfirmModalOpen, ConfirmTransferClassButton, CancelConfirmModalButton, TransferClassButton, TransferLearningButton, DeleteImagesButton }
 }
 
 export default TransferButtons;
