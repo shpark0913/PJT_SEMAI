c704660228ff5aa0c385d40011ec206816a54ddb
YU DAVID <yudavid0611@gmail.com>
2023-05-16 23:46:50 +0900

    :sparkles: [BE] 이상탐지 API 구현 - 더미 데이터로 API 구현 및 테스트 완료 - requirements.txt 수정(openpyxl 추가)
    Former-commit-id: 40e9c36b5009f591b4fb0c7500ad397a612bea54

--- ai/api/main.py
+++ ai/api/main.py
@@ -1,22 +1,20 @@
-from classification import RegNet, Transfer
-from detection import yolo
-from typing import Union
-from fastapi import FastAPI
-from pydantic import BaseModel
 import re
 import os
 import sys
-import torch
-from fastapi import FastAPI, HTTPException
 import time
-from abnormal import check_anormal
+import torch
+import pandas as pd
+from typing import Union
+from detection import yolo
+from fastapi import FastAPI
+from pydantic import BaseModel
+from anomaly.isolation_forest import check_anomaly
+from fastapi import FastAPI, HTTPException
+from classification import RegNet, Transfer
 
 NOW_DIR = os.getcwd()
 sys.path.append(NOW_DIR + '\\classification')
 sys.path.append(NOW_DIR + '\\detection')
-
-# detection
-# clssification
 
 # GPU가 사용 가능한 경우 cuda를 0으로 초기화하여 사용 / GPU가 사용 불가능한 경우 CPU로 초기화하여 CPU 사용
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -24,33 +22,34 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 app = FastAPI()
 
 
+REGEX = re.compile('.jpg|.png|.jpeg|.gif|.bmp|.JPG|.PNG|.JPEG|.GIF|.BMP')
+
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
 
-
 WHEELS = [
     {   
-        'wheel1': 'w1',
-        'lost': 1,
-        'loose': 2,
-        'broken': 3,
+        'wheel_id': 'w1',
+        'lost': 10,
+        'loose': 5,
+        'broken': 8,
     },
     {   
-        'wheel2': 'w2',
+        'wheel_id': 'w2',
         'lost': 1,
-        'loose': 2,
-        'broken': 3,
+        'loose': 20,
+        'broken': 15,
     },
     {
-        'wheel3': 'w3',
-        'lost': 1,
-        'loose': 2,
-        'broken': 3,
+        'wheel_id': 'w3',
+        'lost': 0,
+        'loose': 0,
+        'broken': 0,
     },
     {
-        'wheel4': 'w4',
+        'wheel_id': 'w4',
         'lost': 1,
         'loose': 2,
         'broken': 3,
@@ -63,38 +62,70 @@ def read_root():
     return {"Hello": "SEMES"}
 
 
-REGEX = re.compile('.jpg|.png|.jpeg|.gif|.bmp|.JPG|.PNG|.JPEG|.GIF|.BMP')
+# anomaly로 post 요청이 왔을 때
+@app.post("/anomaly")
+def anomaly_detection():
+
+    # 4개 휠 정보를 받지 못한 경우
+    # if len(wheelAgg) != 4:
+        # detail = {
+        #         'success': False,
+        #         'message': '휠 데이터 개수가 4개가 아닙니다.'
+        #     }
+        #raise HTTPException(status_code=400, detail=detail)
+
+    try:
+        # pandas dataframe화
+        now_wheels = pd.DataFrame(WHEELS)
+        now_wheels.set_index('wheel_id', inplace=True)
+
+        # 이상 탐지 실행
+        anomaly_reuslts = check_anomaly(now_wheels)
+        
+        # 이상탐지 결과를 return할 딕셔너리
+        anomaly = {}
+        
+        for wheel in anomaly_reuslts.index:
+            anomaly.update({wheel: int(anomaly_reuslts.loc[wheel, 'anomaly'])})
+    
+        return {
+            "success": True,
+            'message': '이상탐지 성공',
+            'data': anomaly
+            }
+
+    except Exception:
+        # 이상탐지 작업 중 오류가 발생한 경우
+        detail = {
+            'success': False,
+            'message': '서버 내 오류'
+        }
+        raise HTTPException(status_code=500, detail=detail)
+
 
 # infer로 get 요청이 왔을 때
-
 @app.get("/infer")
 # 휠 이미지 디텍션 후 볼트 분류 함수 실행(쿼리에 담긴 filePath 전달)
 async def detect_classification(filePath: str, binary: bool):
     try:
         # # 추론 시작 시간 설정
-        ###### 여기 풀기
-        # start_test = time.time()
-        # # cropped 된 볼트의 각 bounding box 좌표를 원소로하는 리스트를 받는다.
-        # image, bboxes = yolo.detect_bolt(filePath)
-        # # 확장자 삭제
-        # filePath = re.sub(REGEX, '', filePath)
-        # # 이미지 크롭 후 분류하여 이미지 저장 및 분류 결과 텍스트로 저장
-        # result = RegNet.ImgCrop(filePath, image, bboxes, binary)
-        # # 추론 종료 시간 저장
-        # reasoning_time = round(time.time() - start_test, 3)
-        #######
+        start_test = time.time()
+        # cropped 된 볼트의 각 bounding box 좌표를 원소로하는 리스트를 받는다.
+        image, bboxes = yolo.detect_bolt(filePath)
+        # 확장자 삭제
+        filePath = re.sub(REGEX, '', filePath)
+        # 이미지 크롭 후 분류하여 이미지 저장 및 분류 결과 텍스트로 저장
+        result = RegNet.ImgCrop(filePath, image, bboxes, binary)
+        # 추론 종료 시간 저장
+        reasoning_time = round(time.time() - start_test, 3)
         
-        ## 이상 탐지 ##
-        abnormal_results = check_anormal()
 
         # 데이터를 JSON 형식으로 구성
-        ##### 여기 풀기
-        # data = {
-        #     "markedImage": "WHEEL_RESULT/{}.jpg".format(filePath),
-        #     "bolts": result,
-        #     "word": "저장중"
-        # }
-        #####
+        data = {
+            "markedImage": "WHEEL_RESULT/{}.jpg".format(filePath),
+            "bolts": result,
+            "word": "저장중"
+        }
         
         # 성공적으로 분류 작업을 수행한 경우
         return {
@@ -109,6 +140,7 @@ async def detect_classification(filePath: str, binary: bool):
     except Exception as e:
         # 분류 작업 중 오류가 발생한 경우
         raise HTTPException(status_code=500, detail="서버 내 오류")
+
 
 
 # train으로 get 요청이 왔을 때
