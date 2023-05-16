a0b965fca377cde7a65c5a6ca6d08a4117700e1c
shpark0913 <shpark097754@gmail.com>
2023-05-16 14:40:14 +0900

    :fire: [FE] AI 분석 페이지 표 버그
    - 스크롤이 제대로 생성되지 않기에 표 컴포넌트 재구현 위해 기존 코드 삭제
    
    closes S08P31A301-635
    
    
    Former-commit-id: b2ff8d5df3ffb2e44de2d6ea5255910d0e614445

--- frontend/src/components/PredictPage/PredictTable.tsx
+++ frontend/src/components/PredictPage/PredictTable.tsx
@@ -1,4 +1,3 @@
-import { Button, SemesButton } from "../ButtonComponents";
 import { TBody, TD, TFoot, TH, THead, TR, Table, TableContainer } from "../TableComponents";
 
 import React from "react";
@@ -26,44 +25,7 @@ function PredictTable() {
     { wheelSn: "V30019", loose: 2, lost: 3, broken: 1, normal: 5 },
     { wheelSn: "V30020", loose: 2, lost: 3, broken: 1, normal: 5 },
   ];
-  return (
-    <TableContainer style={{ flexGrow: "1" }}>
-      <Table>
-        <THead>
-          <TR>
-            <TH></TH>
-            <TH>휠 ID</TH>
-            <TH>정상</TH>
-            <TH>유실</TH>
-            <TH>파손</TH>
-            <TH>풀림</TH>
-            {/* <TH>최근 검사 상세</TH> */}
-          </TR>
-        </THead>
-        <TBody className="report-table">
-          {data.map((item, idx) => (
-            <TR key={idx}>
-              <TH className="idxNum">{idx + 1}</TH>
-              <TD>{item.wheelSn}</TD>
-              <TD>{item.normal}</TD>
-              <TD>{item.lost}</TD>
-              <TD>{item.broken}</TD>
-              <TD>{item.loose}</TD>
-              {/* <TD>
-                <Button>상세 보기</Button>
-              </TD> */}
-            </TR>
-          ))}
-        </TBody>
-        <TFoot>
-          <TR>
-            <TD colSpan={5} />
-            <TD colSpan={1}>보기</TD>
-          </TR>
-        </TFoot>
-      </Table>
-    </TableContainer>
-  );
+  return <div>표가 올 곳</div>;
 }
 
 export default PredictTable;
