fdc3ebfea4c109b67e724cc9c043193155c277c6
gyur1kim <qri1114@gmail.com>
2023-05-16 14:23:40 +0900

    :sparkles: [FE] detail absolute로 변경
    - absolute로 변경
    
    closes #S08P31A301-591
    
    
    Former-commit-id: d0f286a56945d267759955884150f3b4337875db

--- frontend/src/components/ReportDetail/ReportDetail.tsx
+++ frontend/src/components/ReportDetail/ReportDetail.tsx
@@ -1,29 +1,60 @@
 import React from "react";
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 import { CloseButton } from "../Modal/ModalComponents";
 import { ReportObjectType } from "../../_utils/Types";
 import DetailTable from "./DetailTable";
 
+const fadeIn = keyframes`
+  0% {
+    right: -100%;
+  }
+  100% {
+    right: 0;
+  }
+`
+const fadeOut = keyframes`
+  0% {
+    right: 0;
+  }
+  100% {
+    right: -100%;
+  }
+`
+
+
 const ReportDetailContainer = styled.div`
   background-color: var(--background-color);
-  width: 100%;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 800px;
   height: 100%;
   overflow-y: auto;
-  padding-left: 30px;
-  margin-left: 30px;
-  transition: width 1000ms ease, transform 1000ms ease, box-shadow 1000ms ease;
+  padding: 30px;
+  //margin-left: 30px;
+  //transform: translateX(150%);
+  transition: width 600ms ease, transform 600ms ease, box-shadow 600ms ease;
   
   &.open{
-    width: 100%;
+    //width: 100%;
     transform: translateX(0);
     box-shadow: -15px 0 10px -10px var(--shadow-color);
+    // animation-duration: 2s;
+    // animation-name: ${fadeIn};
+    // animation-fill-mode: forwards;
+    // animation: ${fadeIn} 1s forwards ease-out;
   }
   &.close {
-    width: 0;
+    //width: 0;
     transform: translateX(200%);
     box-shadow: none;
+    // animation-duration: 2s;
+    // animation-name: ${fadeOut};
+    // animation-fill-mode: forwards;
+    //animation: ${fadeOut} 1s forwards ease-out;
   }
+  
   
 `
 function ReportDetail({handleModalClose, detailInfo, className}: {handleModalClose: any, detailInfo: ReportObjectType, className: string}) {
