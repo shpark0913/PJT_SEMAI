cd985abc94806dc65a9896f6813acedb675845f1
shpark0913 <shpark097754@gmail.com>
2023-05-17 10:14:39 +0900

    :bug: [FE] 대시보드 점검 현황 자동 업데이트되는 버그 해결
    closes #S08P31A301-626
    
    
    Former-commit-id: 4f4051d9b39383dab296b0d5e03ce5b80dc497e5

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -21,13 +21,19 @@ const MainGrid = styled.section`
 function DashboardPage() {
   const BASE_URL = process.env.REACT_APP_BASE_URL;
   const dispatch = useDispatch();
+
+  // OHT 휠 검사 결과 컴포넌트에 props로 보낼 값
   const [wheelDataNew, setWheelDataNew] = useState(null);
+
+  // dashboardData에서 ohtCheckId를 뽑아서 store의 checkId에 넣을 것
+  // checkId가 OHT 휠 검사 결과에 나오는 검사의 Id
   const [dashboardData, setDashboardData] = useState(null);
 
   const ohtCheckId = useSelector(state => {
     return state.dashboard.checkId;
   });
 
+  // isChecked가 false일 때만 OHT 휠 검사 결과 컴포넌트를 업데이트할 것
   const isChecked = useSelector(state => {
     return state.dashboard.inquire;
   });
@@ -43,22 +49,19 @@ function DashboardPage() {
           accesstoken: token,
         },
       });
+
+      // OHT 검사 결과
       sse.addEventListener("state", event => {
         const stateData = JSON.parse(event.data);
         dispatch(
           setSSEState({ ohtSn: stateData.ohtSn, isWheelsProceeding: stateData.isWheelsProceeding }),
         );
       });
+
+      // 대시보드 관련
       sse.addEventListener("dashboard", event => {
         const dashboardData = JSON.parse(event.data);
-        const persistRoot = localStorage.getItem("persist:root");
-        const store = persistRoot ? JSON.parse(persistRoot) : {};
-        const isinquire = JSON.parse(store.dashboard)?.inquire || "";
         setDashboardData(dashboardData);
-        dispatch(setSSEId(dashboardData[0].ohtCheckId));
-        if (isinquire === false) {
-          dispatch(setCheckId(dashboardData[0].ohtCheckId));
-        }
       });
     }
     fetchData();
@@ -67,9 +70,13 @@ function DashboardPage() {
   // SSE에서 데이터를 받아와서 store의 checkId를 변경
   useEffect(() => {
     if (dashboardData) {
+      const persistRoot = localStorage.getItem("persist:root");
+      const store = persistRoot ? JSON.parse(persistRoot) : {};
+      const inquire = JSON.parse(store.dashboard)?.inquire || false;
+      console.log("inquire", inquire);
       const newOHTCheckId = dashboardData[0].ohtCheckId;
       dispatch(setSSEId(newOHTCheckId));
-      if (isChecked === false) {
+      if (inquire === false) {
         dispatch(setCheckId(newOHTCheckId));
       }
     }
@@ -82,7 +89,10 @@ function DashboardPage() {
         try {
           const response = await Axios.get(`${BASE_URL}dashboard/main/${ohtCheckId}`);
           const wheelData = response.data.data;
-          setWheelDataNew(wheelData);
+          if (wheelDataNew !== wheelData) {
+            console.log("wheelData 변신!!!!!!!!!!!!!!!!!!!!!");
+            setWheelDataNew(wheelData);
+          }
         } catch (error) {
           console.log("error", error);
         }
