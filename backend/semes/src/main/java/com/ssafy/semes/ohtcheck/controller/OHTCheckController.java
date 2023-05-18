ce8d6a144cd33812d3998aa4634f962d59add1e8
abin7989 <abin7989@gmail.com>
2023-05-18 13:17:14 +0900

    [BE] 이상탐지 수정 S08P31A301-652
    Former-commit-id: e26d7ceb139a5c06b6cf8360fe1408c7a7b34439

--- backend/semes/src/main/java/com/ssafy/semes/ohtcheck/controller/OHTCheckController.java
+++ backend/semes/src/main/java/com/ssafy/semes/ohtcheck/controller/OHTCheckController.java
@@ -1,10 +1,25 @@
 package com.ssafy.semes.ohtcheck.controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
 
+import com.google.gson.Gson;
 import com.ssafy.semes.dashboard.controller.DashboardController;
+import com.ssafy.semes.report.model.AnomalyEntity;
+import com.ssafy.semes.report.model.AnomalyWheelDTO;
+import com.ssafy.semes.report.model.ReportListResponseDto;
+import com.ssafy.semes.report.model.repository.AnomalyRepository;
+import com.ssafy.semes.report.model.service.ReportService;
+import com.ssafy.semes.wheelcheck.model.WheelCheckEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +55,13 @@ public class OHTCheckController {
 	SseEmitters sseEmitters;
 	@Autowired
 	SlackController slackController;
+	@Autowired
+	ReportService reportService;
+	@Autowired
+	AnomalyRepository anomalyRepository;
+
+	@Value("${Ai-Api-Server-Ip}")
+	private String ip;
 
 	@PostMapping("/{ohtSn}")
 	public ApiResponse<?> checkOht(@PathVariable String ohtSn, MultipartFile[] files){
@@ -63,18 +85,38 @@ public class OHTCheckController {
 				.isProceeding(true)
 				.isWheelsProceeding(new boolean[4]).build();
 
+		List<AnomalyWheelDTO> wheelAgg = new ArrayList<>();
+		List<AnomalyEntity> ano = new ArrayList<>();
 		//바퀴 별 처리
 		for (int i=0;i<4;i++) {
 			MultipartFile file = files[i];
 			try {
-				wheelCheckService.checkWheel(file,ohtFileName, WheelPosition.values()[i],ohtCheck);
+				WheelCheckEntity wheelCheckEntity = wheelCheckService.checkWheel(file,ohtFileName, WheelPosition.values()[i],ohtCheck);
+				ReportListResponseDto reportDetail  = reportService.findReportDetail(wheelCheckEntity.getWheelHistoryId());
+				wheelAgg.add(AnomalyWheelDTO.builder()
+						.wheel_id(reportDetail.getOhtSn())
+						.lost(reportDetail.getTotalOutCount())
+						.broken(reportDetail.getTotalLoseCount())
+						.loose(reportDetail.getTotalLooseCount())
+						.build());
 
+				ano.add(AnomalyEntity.builder().ohtSn(reportDetail.getOhtSn())
+						.wheelCheckId(reportDetail.getWheelCheckId())
+						.totalLooseCount(reportDetail.getTotalLooseCount())
+						.totalLoseCount(reportDetail.getTotalLoseCount())
+						.totalGoodCount(reportDetail.getTotalGoodCount())
+						.totalOutCount(reportDetail.getTotalOutCount())
+						.wheelPosition(reportDetail.getWheelPosition())
+						.build());
 			}catch (ConnectException e){
 				log.info("OHTCheckController checkOht error AI server 오류");
 				return ApiResponse.error(ErrorCode.AI_SERVER_CONNECTION_ERROR);
 			} catch (IOException | InterruptedException e) {
 				slackController.errorSend("OHTCheckController checkOht error checkWheel :"+e.getMessage());
 				log.info("OHTCheckController checkOht error checkWheel");
+				throw new RuntimeException(e);
+			} catch (Exception e) {
+				log.info("OHTCheckController checkOht error");
 				throw new RuntimeException(e);
 			}
 			// 끝날 때마다 검사상태 SSE로 보내주기
@@ -83,6 +125,42 @@ public class OHTCheckController {
 			DashboardController.nowNum = i;
 			sseEmitters.showProcessStatus(processStatusDto);
 		}
+		String json = new Gson().toJson(wheelAgg);
+		try {
+			URL url = new URL("http://"+ip+":8000/anomaly");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Content-Type","applicaiton/json;utf-8");
+			con.setRequestProperty("Accept","application/json");
+			con.setDoOutput(true);
+			try(OutputStream os = con.getOutputStream()){
+				byte[] input = json.getBytes("utf-8");
+				os.write(input, 0, input.length);
+			}
+			try(BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(),"utf-8"))){
+				StringBuilder response = new StringBuilder();
+				String responseLine = null;
+				while((responseLine = br.readLine()) != null){
+					response.append(responseLine.trim());
+				}
+				Gson gson = new Gson();
+				ApiResponse map;
+				map = gson.fromJson(response.toString(),ApiResponse.class);
+				StringTokenizer st = new StringTokenizer(map.getData().toString(),"{=, }");
+				int temp;
+				for(int i = 0 ; i < 4; i++){
+					st.nextToken();
+					temp =(int) Double.parseDouble(st.nextToken());
+					ano.get(i).setAnomalyFlag((int) temp);
+				}
+
+				anomalyRepository.saveAll(ano);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+
 		ohtCheckService.updateOhtCheckEndDate(ohtCheck);
 		slackController.successSend(processStatusDto.getOhtSn()+"번 OHT 검사 종료");
 		return ApiResponse.success(SuccessCode.CHECK_OHT_COMPLETE);
