446e6f956c869ef74239fd85aee215d9c1196466
gyur1kim <qri1114@gmail.com>
2023-05-18 10:29:54 +0900

    :bug: [FE] input의 defaultValue가 변하지 않는 문제 해결
    - useEffect를 이용해 query가 변하면 redux의 queryObj 변경
    
    closes #S08P31A301-609
    
    
    Former-commit-id: 5cc3cfbc8743f4a863bcf9d013e1dd67eea2fc45

--- frontend/src/components/ReportPage/InputErrorFlag.tsx
+++ frontend/src/components/ReportPage/InputErrorFlag.tsx
@@ -4,7 +4,7 @@ import {Label} from "./FilterComponents";
 
 function InputErrorFlag({handleSubmit}: {handleSubmit: (e: React.ChangeEvent<HTMLInputElement>) => void}) {
   const theme=useAppSelector(state => state.theme.theme);
-  const { errorFlag } = useAppSelector(state => state.reportPage.queryObj)
+  const { errorFlag } = useAppSelector(state => state.reportPage.queryObj);
   return (
     <Label theme={theme}> 오류 기록만 조회
       <input type="checkbox" checked={errorFlag === "1"} name="errorFlag" value={1} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSubmit(e)} />
