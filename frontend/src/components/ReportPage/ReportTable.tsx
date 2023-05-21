c1182d81e3c0766d3ee8aa66214894386c6e2c4b
gyur1kim <qri1114@gmail.com>
2023-05-22 01:09:53 +0900

    :recycle: [FE] 레포트 테이블
    Former-commit-id: f43b6e72d71f648007194d3daf1df638db69e8ac

--- frontend/src/components/ReportPage/ReportTable.tsx
+++ frontend/src/components/ReportPage/ReportTable.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { useRouteLoaderData } from "react-router-dom";
 
-import { ReportTableProps } from "../../_utils/Types";
+import { useAppSelector } from "../../_hooks/hooks";
+import { ReportLoaderType, ReportTableProps } from "../../_utils/Types";
 import useDate from "../../_hooks/useDate";
 
-import {Table, TableContainer, TBody, TD, TFoot, TH, THead, TR} from "../TableComponents";
-import {useAppSelector} from "../../_hooks/hooks";
+import { Table, TableContainer, TBody, TD, TFoot, TH, THead, TR } from "../TableComponents";
 
 function ReportTable({ handleModalOpen }: ReportTableProps) {
-  let data: any = useRouteLoaderData("reportLists");
-  let { result, totalPage } = data;
+  const { result, totalPage } = useRouteLoaderData("reportLists") as ReportLoaderType;
   let { wheelReportId, dateFormat, timeFormat } = useDate();
-  const { queryObj } = useAppSelector(state => state.reportPage);
-  let nowPage = queryObj.page
+  let { page } = useAppSelector(state => state.reportPage.queryObj);
+  let nowPage:number = Number(page);
 
   return (
     <TableContainer>
@@ -28,13 +27,15 @@ function ReportTable({ handleModalOpen }: ReportTableProps) {
             <TH>검사 결과</TH>
             <TH>기준값</TH>
             <TH>결과값</TH>
-            {/*<TH>상세보기</TH>*/}
           </TR>
         </THead>
         <TBody className="report-table">
           { result.map((report:any, idx:number) =>
-              <TR key={`${report.ohtSn}-${report.wheelPosition}-${wheelReportId(report.wheelCheckDate.slice(0, 6))}`} onClick={(e: React.MouseEvent<HTMLTableRowElement>) => handleModalOpen(e, report.wheelCheckId)} NG={11 - report.boltGoodCount} >
-                <TH className="idxNum">{((parseInt(nowPage) - 1) * 20) + idx + 1}</TH>
+              <TR key={`${report.ohtSn}-${report.wheelPosition}-${wheelReportId(report.wheelCheckDate.slice(0, 6))}`}
+                  onClick={(e: React.MouseEvent<HTMLTableRowElement>) => handleModalOpen(e, report.wheelCheckId)}
+                  NG={11 - report.boltGoodCount}
+              >
+                <TH className="idxNum">{((nowPage - 1) * 20) + idx + 1}</TH>
                 <TD>{`${report.ohtSn}-${report.wheelPosition}-${wheelReportId(report.wheelCheckDate.slice(0, 6))}`}</TD>
                 <TD>{`${dateFormat(report.wheelCheckDate.slice(0, 3))}`}</TD>
                 <TD>{`${timeFormat(report.wheelCheckDate.slice(3, 6))}`}</TD>
@@ -43,14 +44,16 @@ function ReportTable({ handleModalOpen }: ReportTableProps) {
                 <TD>{report.boltGoodCount === 11 ? "정상" : "NG"}</TD>
                 <TD>11</TD>
                 <TD>{report.boltGoodCount}</TD>
-              {/*<TD><Button width="65px" height="23px" onClick={(e: React.MouseEvent<HTMLButtonElement>) => handleModalOpen(e, report.wheelCheckId)}>{report.wheelCheckId}</Button></TD>*/}
               </TR>
           ) }
         </TBody>
         <TFoot>
           <TR>
             <TD colSpan={7} />
-            <TD colSpan={2}>보기 {(Number(nowPage)-1)*20+1}-{Number(nowPage)*20 > totalPage? totalPage : Number(nowPage)*20} / <span style={{color: "var(--emphasize-color)", fontWeight: "bold", fontSize: "16px"}}>{ totalPage }</span></TD>
+            <TD colSpan={2}>
+              {(nowPage-1)*20+1}-{nowPage*20 > totalPage? totalPage : nowPage*20} /
+              <span style={{color: "var(--emphasize-color)", fontWeight: "bold", fontSize: "16px"}}> { totalPage }</span>
+            </TD>
           </TR>
         </TFoot>
       </Table>
