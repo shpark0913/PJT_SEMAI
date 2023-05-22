605348661283ce78f4a89b90444c571105962938
gyur1kim <qri1114@gmail.com>
2023-05-22 17:37:45 +0900

    :recycle: [FE] report detail redux에서 관리
    Former-commit-id: 142d48a8cd8a11a0b6b6e1bc74da9b38443ecd58

--- frontend/src/components/ReportDetail/DetailTable.tsx
+++ frontend/src/components/ReportDetail/DetailTable.tsx
@@ -1,170 +1,164 @@
 import React, { useState } from "react";
-import { TBody, TD, TH, TR, Table, TableContainer } from "../TableComponents";
-import { setCheckId, setInquire, setWheelImgUrl } from "../../_store/slices/dashboardSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
+import { setWheelImgUrl } from "../../_store/slices/dashboardSlice";
+import useDate from "../../_hooks/useDate";
+import ImageUrl from "../../_utils/ImageUrl";
+import {useAppSelector} from "../../_hooks/hooks";
 
 import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
-import { CloseButton } from "../Modal/ModalComponents";
 import CloseIcon from "@mui/icons-material/Close";
 import Fade from "@mui/material/Fade";
-import ImageUrl from "../../_utils/ImageUrl";
 import Modal from "@mui/material/Modal";
-import { ReactComponent as RefreshBtn } from "../../assets/refreshBtn.svg";
-import { ReportObjectType } from "../../_utils/Types";
-import Title from "../Title";
-import styled from "styled-components";
-import useDate from "../../_hooks/useDate";
 
-function DetailTable({ detailInfo }: { detailInfo: ReportObjectType }) {
-  let { wheelReportId, dateFormat, timeFormat } = useDate();
-  const imgUrl = useSelector((state: any) => {
-    return state.dashboard.imgUrl;
-  });
-  const IMG_URL = process.env.REACT_APP_IMG_URL;
-  const indexList = [0, 1, 2, 3];
+import { CloseButton } from "../Modal/ModalComponents";
+import { TBody, TD, TH, TR, Table, TableContainer } from "../TableComponents";
+
+function DetailTable() {
   const dispatch = useDispatch();
+  let { wheelReportId, dateFormat, timeFormat } = useDate();
+  let { imgUrl } = useAppSelector(state => state.dashboard)
+  const { reportDetail } = useAppSelector(state => state.reportPage);
+  const { ohtSn, boltGoodCount, boltOutCount, boltLoseCount, wheelCheckDate, wheelPosition, markingUrl, originUrl  } = reportDetail;
+
   const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const divStyle = {
-    width: "45vw",
-    height: "45vw",
+    width: "100%",
+    height: "100%",
     backgroundImage: `url(${imgUrl})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
   };
+
   return (
-    <TableContainer style={{ width: "100%" }}>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        slots={{ backdrop: Backdrop }}
-        slotProps={{
-          backdrop: {
-            timeout: 500,
-          },
-        }}
-      >
-        <Fade in={open}>
-          <Box
-            sx={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              width: "400",
-              backgroundColor: "white",
-              border: "2px solid #000",
-              boxShadow: "24",
-              p: "4",
-              color: "black",
+        <TableContainer style={{ width: "100%" }}>
+          {/* 이미지 상세보기 모달창 */}
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            slots={{ backdrop: Backdrop }}
+            slotProps={{
+              backdrop: {
+                timeout: 500,
+              },
             }}
           >
-            <div style={divStyle}>
-              <CloseButton
-                style={{ position: "absolute", right: "1.5vw", top: "1.5vw", color: "#A3D1FF" }}
-                onClick={handleClose}
+            <Fade in={open}>
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  width: "80vh",
+                  height: "80vh",
+                  backgroundColor: "white",
+                  border: "2px solid #000",
+                  boxShadow: "24",
+                  p: "4",
+                  color: "black",
+                }}
               >
-                <CloseIcon sx={{ width: "35px", height: "35px" }} />
-              </CloseButton>
-            </div>
-          </Box>
-        </Fade>
-      </Modal>
-      <Table className="detail">
-        <TBody>
-          <TR>
-            <TH>검사 ID</TH>
-            <TD colSpan={2}>{`${detailInfo.ohtSn}-${detailInfo.wheelPosition}-${wheelReportId(
-              detailInfo.wheelCheckDate.slice(0, 6),
-            )}`}</TD>
-          </TR>
-          <TR>
-            <TH>일자</TH>
-            <TD colSpan={2}>{`${dateFormat(detailInfo.wheelCheckDate.slice(0, 3))}`}</TD>
-          </TR>
-          <TR>
-            <TH>시간</TH>
-            <TD colSpan={2}>{`${timeFormat(detailInfo.wheelCheckDate.slice(3, 6))}`}</TD>
-          </TR>
-          <TR>
-            <TH>호기</TH>
-            <TD colSpan={2}>{detailInfo.ohtSn}</TD>
-          </TR>
-          <TR>
-            <TH>휠 위치</TH>
-            <TD colSpan={2}>{detailInfo.wheelPosition}</TD>
-          </TR>
-          <TR>
-            <TH>판정 결과</TH>
-            <TD colSpan={2}>{detailInfo.boltGoodCount === 11 ? "정상" : "NG"}</TD>
-          </TR>
-          <TR>
-            <TH>기준값</TH>
-            <TD colSpan={2}>11</TD>
-          </TR>
-          <TR>
-            <TH rowSpan={4}>결과값</TH>
-            <TH>양호</TH>
-            <TD>{detailInfo.boltGoodCount}</TD>
-          </TR>
-          <TR>
-            <TH>유실</TH>
-            <TD>{detailInfo.boltOutCount}</TD>
-          </TR>
-          <TR>
-            <TH>파단</TH>
-            <TD>{detailInfo.boltLoseCount}</TD>
-          </TR>
-          <TR>
-            <TH>풀림</TH>
-            <TD>
-              {11 - (detailInfo.boltLoseCount + detailInfo.boltOutCount + detailInfo.boltGoodCount)}
-            </TD>
-            {/* <TD>
-              {detailInfo.boltLoseCount && detailInfo.boltOutCount && detailInfo.boltGoodCount
-                ? 11 -
-                  (detailInfo.boltLoseCount + detailInfo.boltOutCount + detailInfo.boltGoodCount)
-                : 0}
-            </TD> */}
-          </TR>
-          <TR>
-            <TH>Marked Image</TH>
-            <TD colSpan={2}>
-              <img
-                width="100%"
-                src={ImageUrl(detailInfo.markingUrl)}
-                alt=""
-                onClick={() => {
-                  dispatch(setWheelImgUrl(`${IMG_URL}${detailInfo.markingUrl}`));
-                  setOpen(true);
-                }}
-                style={{ cursor: "pointer" }}
-              />
-            </TD>
-          </TR>
-          <TR>
-            <TH>Raw Image</TH>
-            <TD colSpan={2}>
-              <img
-                width="100%"
-                src={ImageUrl(detailInfo.originUrl)}
-                alt=""
-                onClick={() => {
-                  dispatch(setWheelImgUrl(`${ImageUrl(detailInfo.originUrl)}`));
-                  setOpen(true);
-                }}
-                style={{ cursor: "pointer" }}
-              />
-            </TD>
-          </TR>
-        </TBody>
-      </Table>
-    </TableContainer>
+                <div style={divStyle}>
+                  <CloseButton
+                    style={{ position: "absolute", right: "1.5vw", top: "1.5vh", color: "#A3D1FF" }}
+                    onClick={handleClose}
+                  >
+                    <CloseIcon sx={{ width: "35px", height: "35px" }} />
+                  </CloseButton>
+                </div>
+              </Box>
+            </Fade>
+          </Modal>
+
+          {/* 테이블 */}
+          <Table className="detail">
+            <TBody>
+              <TR>
+                <TH>검사 ID</TH>
+                <TD colSpan={2}>{`${ ohtSn }-${wheelPosition}-${wheelReportId(wheelCheckDate.slice(0, 6))}`}</TD>
+              </TR>
+              <TR>
+                <TH>일자</TH>
+                <TD colSpan={2}>{`${dateFormat(wheelCheckDate.slice(0, 3))}`}</TD>
+              </TR>
+              <TR>
+                <TH>시간</TH>
+                <TD colSpan={2}>{`${timeFormat(wheelCheckDate.slice(3, 6))}`}</TD>
+              </TR>
+              <TR>
+                <TH>호기</TH>
+                <TD colSpan={2}>{ohtSn}</TD>
+              </TR>
+              <TR>
+                <TH>휠 위치</TH>
+                <TD colSpan={2}>{wheelPosition}</TD>
+              </TR>
+              <TR>
+                <TH>판정 결과</TH>
+                <TD colSpan={2}>{boltGoodCount === 11 ? "정상" : "NG"}</TD>
+              </TR>
+              <TR>
+                <TH>기준값</TH>
+                <TD colSpan={2}>11</TD>
+              </TR>
+              <TR>
+                <TH rowSpan={4}>결과값</TH>
+                <TH>양호</TH>
+                <TD>{boltGoodCount}</TD>
+              </TR>
+              <TR>
+                <TH>유실</TH>
+                <TD>{boltOutCount}</TD>
+              </TR>
+              <TR>
+                <TH>파단</TH>
+                <TD>{boltLoseCount}</TD>
+              </TR>
+              <TR>
+                <TH>풀림</TH>
+                <TD>
+                  {11 - (boltLoseCount + boltOutCount + boltGoodCount)}
+                </TD>
+              </TR>
+              <TR>
+                <TH>Marked Image</TH>
+                <TD colSpan={2}>
+                  <img
+                    width="100%"
+                    src={ImageUrl(markingUrl)}
+                    alt=""
+                    onClick={() => {
+                      dispatch(setWheelImgUrl(ImageUrl(reportDetail.markingUrl)));
+                      setOpen(true);
+                    }}
+                    style={{ cursor: "pointer" }}
+                  />
+                </TD>
+              </TR>
+              <TR>
+                <TH>Raw Image</TH>
+                <TD colSpan={2}>
+                  <img
+                    width="100%"
+                    src={ImageUrl(reportDetail.originUrl)}
+                    alt=""
+                    onClick={() => {
+                      dispatch(setWheelImgUrl(ImageUrl(originUrl)));
+                      setOpen(true);
+                    }}
+                    style={{ cursor: "pointer" }}
+                  />
+                </TD>
+              </TR>
+            </TBody>
+          </Table>
+        </TableContainer>
   );
 }
 
--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,10 +1,9 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo } from "react";
 import { useLoaderData, useSearchParams } from "react-router-dom";
 
-import { setQueryObj } from "../_store/slices/reportPageSlice";
-import { ReportLoaderType, ReportObjectType } from "../_utils/Types";
-import Axios from "../_utils/Axios";
-import { useAppDispatch } from "../_hooks/hooks";
+import {setDetailClose, setQueryObj} from "../_store/slices/reportPageSlice";
+import { ReportLoaderType } from "../_utils/Types";
+import {useAppDispatch, useAppSelector} from "../_hooks/hooks";
 
 import Pagination from "../components/ReportPage/Pagination";
 import ReportDetail from "../components/ReportDetail/ReportDetail";
@@ -22,65 +21,31 @@ function ReportPage() {
   let { totalPage } = useLoaderData() as ReportLoaderType;    // 서버에서 받아온 값, 총 데이터 개수
   let [query] = useSearchParams();
   useEffect(() => {
+    dispatch(setDetailClose())
+  }, []);
+  useEffect(() => {
     dispatch(setQueryObj(Object.fromEntries(query)));     // 초기에 params를 redux에 저장하기
   }, []);
+  let { isDetailOpen } = useAppSelector(state => state.reportPage)
 
   // 데이터를 20개씩 나누었을 때 하단에 총 몇 개의 page를 표기해야 하는지 계산
   let paginationTotalPage = useMemo(() => Math.ceil(totalPage / 20), [totalPage]);
 
-  // =================== 모달 관련 ===================
-  let [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  let [detailInfo, setDetailInfo] = useState<ReportObjectType>({
-    wheelCheckDate: [2023, 5, 2, 4, 32, 10],
-    boltGoodCount: 0,
-    boltLoseCount: 0,
-    boltOutCount: 0,
-  });
-  /** 모달이 열리면 실행되는 함수 */
-  const handleModalOpen = useCallback(
-    async (e: React.MouseEvent<HTMLTableRowElement>, wheelCheckId: number) => {
-      e.preventDefault();
-      let reportDetail: ReportObjectType = {
-        wheelCheckDate: [2023, 5, 2, 4, 32, 10],
-        boltGoodCount: 0,
-        boltLoseCount: 0,
-        boltOutCount: 0,
-      };
-      try {
-        let response = await Axios.get(`report/detail/${wheelCheckId}`);
-        reportDetail = response.data.data;
-      } catch (err) {
-        console.log(err);
-      }
-      await setDetailInfo(reportDetail);
-      setIsModalOpen(true);
-    },
-    [],
-  );
-
-  const handleModalClose = useCallback(() => {
-    setIsModalOpen(false);
-  }, []);
-
   return (
     <ReportSection>
-      <ReportFormContainer className={isModalOpen ? "open" : "close"}>
+      <ReportFormContainer className={isDetailOpen ? "open" : "close"}>
         <ReportForm replace={true} method="GET">
           <FormInputs />
           { totalPage?
             ( <>
-              <ReportTable handleModalOpen={handleModalOpen} />
+              <ReportTable />
               <Pagination paginationTotalPage={paginationTotalPage} />
             </> )
             : <NoData>데이터가 존재하지 않습니다.</NoData> }
         </ReportForm>
       </ReportFormContainer>
 
-      <ReportDetail
-        className={isModalOpen ? "open" : "close"}
-        handleModalClose={handleModalClose}
-        detailInfo={detailInfo}
-      ></ReportDetail>
+      <ReportDetail className={isDetailOpen ? "open" : "close"} />
     </ReportSection>
   );
 }
